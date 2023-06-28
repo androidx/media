52706b2eb44f5831390c90e34ad4afbd90b0a2f3
tianyifeng <tianyifeng@google.com>
2023-06-28 10:41:49 +0000

    Ensure that ShuffleOrder has the same length as the current playlist
    Add a fail-fast check in `ExoPlayerImpl` to ensure the equality of the lengths of `ShuffleOrder` and the current playlist. Also improve the documentation of `setShuffleOrder(ShuffleOrder)` with explicit instruction on this.
    
    Issue: androidx/media#480
    
    #minor-release
    
    PiperOrigin-RevId: 544009359
    (cherry picked from commit d895a46b280f1f9764f63a5fc1ba16ab2727191e)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayer.java
@@ -1469,6 +1469,9 @@ public interface ExoPlayer extends Player {
   /**
    * Sets the shuffle order.
    *
+   * <p>The {@link ShuffleOrder} passed must have the same length as the current playlist ({@link
+   * Player#getMediaItemCount()}).
+   *
    * @param shuffleOrder The shuffle order.
    */
   @UnstableApi
