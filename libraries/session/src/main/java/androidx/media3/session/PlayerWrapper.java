google/ExoPlayer

commit c71e4bf1ffd4a0a0c631c27e4faa9480700e2c5f

Extend Player interface, overloading 4 device-volume methods with flags
Previously, ExoPlayerImpl had volume flags hardcoded to SHOW_UI, but now the developer can choose what happens on volume change. The old methods have been deprecated.

PiperOrigin-RevId: 523974358

libraries/session/src/main/java/androidx/media3/session/PlayerWrapper.java
@@ -736,18 +736,28 @@ import java.util.List;
     return isCommandAvailable(Player.COMMAND_GET_DEVICE_VOLUME) && isDeviceMuted();
   }
 
+  /**
+   * @deprecated Use {@link #setDeviceVolume(int, int)} instead.
+   */
+  @SuppressWarnings("deprecation") // Forwarding to deprecated method
+  @Deprecated
   @Override
   public void setDeviceVolume(int volume) {
     verifyApplicationThread();
     super.setDeviceVolume(volume);
   }
 
-  public void setDeviceVolumeIfCommandAvailable(int volume) {
-    if (isCommandAvailable(COMMAND_SET_DEVICE_VOLUME)) {
-      setDeviceVolume(volume);
-    }
+  @Override
+  public void setDeviceVolume(int volume, @C.VolumeFlags int flags) {
+    verifyApplicationThread();
+    super.setDeviceVolume(volume, flags);
   }
 
+  /**
+   * @deprecated Use {@link #increaseDeviceVolume(int)} instead.
+   */
+  @SuppressWarnings("deprecation") // Forwarding to deprecated method
+  @Deprecated
   @Override
   public void increaseDeviceVolume() {
     verifyApplicationThread();
@@ -755,15 +765,43 @@ import java.util.List;
   }
 
   @Override
+  public void increaseDeviceVolume(@C.VolumeFlags int flags) {
+    verifyApplicationThread();
+    super.increaseDeviceVolume(flags);
+  }
+
+  /**
+   * @deprecated Use {@link #decreaseDeviceVolume(int)} instead.
+   */
+  @SuppressWarnings("deprecation") // Forwarding to deprecated method
+  @Deprecated
+  @Override
   public void decreaseDeviceVolume() {
     verifyApplicationThread();
     super.decreaseDeviceVolume();
   }
 
   @Override
+  public void decreaseDeviceVolume(@C.VolumeFlags int flags) {
+    verifyApplicationThread();
+    super.decreaseDeviceVolume(flags);
+  }
+
+  /**
+   * @deprecated Use {@link #setDeviceMuted(boolean, int)} instead.
+   */
+  @SuppressWarnings("deprecation") // Forwarding to deprecated method
+  @Deprecated
+  @Override
   public void setDeviceMuted(boolean muted) {
     verifyApplicationThread();
     super.setDeviceMuted(muted);
+  }
+
+  @Override
+  public void setDeviceMuted(boolean muted, @C.VolumeFlags int flags) {
+    verifyApplicationThread();
+    super.setDeviceMuted(muted, flags);
   }
 
   @Override
@@ -974,6 +1012,7 @@ import java.util.List;
   }
 
   @Nullable
+  @SuppressWarnings("deprecation") // Backwards compatibility with old volume commands
   public VolumeProviderCompat createVolumeProviderCompat() {
     if (getDeviceInfo().playbackType == DeviceInfo.PLAYBACK_TYPE_LOCAL) {
       return null;
@@ -988,10 +1027,23 @@ import java.util.List;
     }
     Handler handler = new Handler(getApplicationLooper());
     int currentVolume = getDeviceVolumeWithCommandCheck();
+    int legacyVolumeFlag = C.VOLUME_FLAG_SHOW_UI;
     return new VolumeProviderCompat(volumeControlType, getDeviceInfo().maxVolume, currentVolume) {
       @Override
       public void onSetVolumeTo(int volume) {
-        postOrRun(handler, () -> setDeviceVolumeIfCommandAvailable(volume));
+        postOrRun(
+            handler,
+            () -> {
+              if (!isCommandAvailable(COMMAND_SET_DEVICE_VOLUME)
+                  && !isCommandAvailable(COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS)) {
+                return;
+              }
+              if (isCommandAvailable(COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS)) {
+                setDeviceVolume(volume, legacyVolumeFlag);
+              } else {
+                setDeviceVolume(volume);
+              }
+            });
       }
 
       @Override
@@ -999,24 +1051,45 @@ import java.util.List;
         postOrRun(
             handler,
             () -> {
-              if (!isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME)) {
+              if (!isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME)
+                  && !isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
                 return;
               }
               switch (direction) {
                 case AudioManager.ADJUST_RAISE:
-                  increaseDeviceVolume();
+                  if (isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
+                    increaseDeviceVolume(legacyVolumeFlag);
+                  } else {
+                    increaseDeviceVolume();
+                  }
                   break;
                 case AudioManager.ADJUST_LOWER:
-                  decreaseDeviceVolume();
+                  if (isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
+                    decreaseDeviceVolume(legacyVolumeFlag);
+                  } else {
+                    decreaseDeviceVolume();
+                  }
                   break;
                 case AudioManager.ADJUST_MUTE:
-                  setDeviceMuted(true);
+                  if (isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
+                    setDeviceMuted(true, legacyVolumeFlag);
+                  } else {
+                    setDeviceMuted(true);
+                  }
                   break;
                 case AudioManager.ADJUST_UNMUTE:
-                  setDeviceMuted(false);
+                  if (isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
+                    setDeviceMuted(false, legacyVolumeFlag);
+                  } else {
+                    setDeviceMuted(false);
+                  }
                   break;
                 case AudioManager.ADJUST_TOGGLE_MUTE:
-                  setDeviceMuted(!isDeviceMutedWithCommandCheck());
+                  if (isCommandAvailable(COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
+                    setDeviceMuted(!isDeviceMutedWithCommandCheck(), legacyVolumeFlag);
+                  } else {
+                    setDeviceMuted(!isDeviceMutedWithCommandCheck());
+                  }
                   break;
                 default:
                   Log.w(

libraries/cast/build.gradle
@@ -14,7 +14,7 @@
 apply from: "$gradle.ext.androidxMediaSettingsDir/common_library_config.gradle"
 
 dependencies {
-    api 'com.google.android.gms:play-services-cast-framework:21.2.0'
+    api 'com.google.android.gms:play-services-cast-framework:21.3.0'
     implementation 'androidx.annotation:annotation:' + androidxAnnotationVersion
     implementation project(modulePrefix + 'lib-common')
     compileOnly 'org.checkerframework:checker-qual:' + checkerframeworkVersion

libraries/cast/src/main/java/androidx/media3/cast/CastPlayer.java
@@ -755,21 +755,49 @@ public final class CastPlayer extends BasePlayer {
     return false;
   }
 
-  /** This method is not supported and does nothing. */
+  /**
+   * @deprecated Use {@link #setDeviceVolume(int, int)} instead.
+   */
+  @Deprecated
   @Override
   public void setDeviceVolume(int volume) {}
 
   /** This method is not supported and does nothing. */
   @Override
+  public void setDeviceVolume(int volume, @C.VolumeFlags int flags) {}
+
+  /**
+   * @deprecated Use {@link #increaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
+  @Override
   public void increaseDeviceVolume() {}
 
   /** This method is not supported and does nothing. */
+  @Override
+  public void increaseDeviceVolume(@C.VolumeFlags int flags) {}
+
+  /**
+   * @deprecated Use {@link #decreaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
   @Override
   public void decreaseDeviceVolume() {}
 
   /** This method is not supported and does nothing. */
   @Override
+  public void decreaseDeviceVolume(@C.VolumeFlags int flags) {}
+
+  /**
+   * @deprecated Use {@link #setDeviceMuted(boolean, int)} instead.
+   */
+  @Deprecated
+  @Override
   public void setDeviceMuted(boolean muted) {}
+
+  /** This method is not supported and does nothing. */
+  @Override
+  public void setDeviceMuted(boolean muted, @C.VolumeFlags int flags) {}
 
   // Internal methods.
 

libraries/common/src/main/java/androidx/media3/common/C.java
@@ -410,6 +410,37 @@ public final class C {
   @UnstableApi public static final int STREAM_TYPE_DEFAULT = STREAM_TYPE_MUSIC;
 
   /**
+   * Volume flags to be used when setting or adjusting device volume. The value can be either 0 or a
+   * combination of the following flags: {@link #VOLUME_FLAG_SHOW_UI}, {@link
+   * #VOLUME_FLAG_ALLOW_RINGER_MODES}, {@link #VOLUME_FLAG_PLAY_SOUND}, {@link
+   * #VOLUME_FLAG_REMOVE_SOUND_AND_VIBRATE}, {@link #VOLUME_FLAG_VIBRATE}.
+   */
+  @Documented
+  @Retention(RetentionPolicy.SOURCE)
+  @Target({TYPE_USE})
+  @IntDef(
+      flag = true,
+      value = {
+        VOLUME_FLAG_SHOW_UI,
+        VOLUME_FLAG_ALLOW_RINGER_MODES,
+        VOLUME_FLAG_PLAY_SOUND,
+        VOLUME_FLAG_REMOVE_SOUND_AND_VIBRATE,
+        VOLUME_FLAG_VIBRATE,
+      })
+  public @interface VolumeFlags {}
+  /** See {@link AudioManager#FLAG_SHOW_UI}. */
+  public static final int VOLUME_FLAG_SHOW_UI = AudioManager.FLAG_SHOW_UI;
+  /** See {@link AudioManager#FLAG_ALLOW_RINGER_MODES}. */
+  public static final int VOLUME_FLAG_ALLOW_RINGER_MODES = AudioManager.FLAG_ALLOW_RINGER_MODES;
+  /** See {@link AudioManager#FLAG_PLAY_SOUND}. */
+  public static final int VOLUME_FLAG_PLAY_SOUND = AudioManager.FLAG_PLAY_SOUND;
+  /** See {@link AudioManager#FLAG_REMOVE_SOUND_AND_VIBRATE}. */
+  public static final int VOLUME_FLAG_REMOVE_SOUND_AND_VIBRATE =
+      AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE;
+  /** See {@link AudioManager#FLAG_VIBRATE}. */
+  public static final int VOLUME_FLAG_VIBRATE = AudioManager.FLAG_VIBRATE;
+
+  /**
    * Content types for audio attributes. One of:
    *
    * <ul>

libraries/common/src/main/java/androidx/media3/common/ForwardingPlayer.java
@@ -846,28 +846,64 @@ public class ForwardingPlayer implements Player {
     return player.isDeviceMuted();
   }
 
-  /** Calls {@link Player#setDeviceVolume(int)} on the delegate. */
+  /**
+   * @deprecated Use {@link #setDeviceVolume(int, int)} instead.
+   */
+  @Deprecated
   @Override
   public void setDeviceVolume(int volume) {
     player.setDeviceVolume(volume);
   }
 
-  /** Calls {@link Player#increaseDeviceVolume()} on the delegate. */
+  /** Calls {@link Player#setDeviceVolume(int, int)} on the delegate. */
+  @Override
+  public void setDeviceVolume(int volume, @C.VolumeFlags int flags) {
+    player.setDeviceVolume(volume, flags);
+  }
+
+  /**
+   * @deprecated Use {@link #increaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
   @Override
   public void increaseDeviceVolume() {
     player.increaseDeviceVolume();
   }
 
-  /** Calls {@link Player#decreaseDeviceVolume()} on the delegate. */
+  /** Calls {@link Player#increaseDeviceVolume(int)} on the delegate. */
+  @Override
+  public void increaseDeviceVolume(@C.VolumeFlags int flags) {
+    player.increaseDeviceVolume(flags);
+  }
+
+  /**
+   * @deprecated Use {@link #decreaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
   @Override
   public void decreaseDeviceVolume() {
     player.decreaseDeviceVolume();
   }
 
-  /** Calls {@link Player#setDeviceMuted(boolean)} on the delegate. */
+  /** Calls {@link Player#decreaseDeviceVolume(int)} on the delegate. */
+  @Override
+  public void decreaseDeviceVolume(@C.VolumeFlags int flags) {
+    player.decreaseDeviceVolume(flags);
+  }
+
+  /**
+   * @deprecated Use {@link #setDeviceMuted(boolean, int)} instead.
+   */
+  @Deprecated
   @Override
   public void setDeviceMuted(boolean muted) {
     player.setDeviceMuted(muted);
+  }
+
+  /** Calls {@link Player#setDeviceMuted(boolean, int)} on the delegate. */
+  @Override
+  public void setDeviceMuted(boolean muted, @C.VolumeFlags int flags) {
+    player.setDeviceMuted(muted, flags);
   }
 
   /** Returns the {@link Player} to which operations are forwarded. */

libraries/common/src/main/java/androidx/media3/common/Player.java
@@ -380,7 +380,9 @@ public interface Player {
         COMMAND_GET_DEVICE_VOLUME,
         COMMAND_SET_VOLUME,
         COMMAND_SET_DEVICE_VOLUME,
+        COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS,
         COMMAND_ADJUST_DEVICE_VOLUME,
+        COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS,
         COMMAND_SET_VIDEO_SURFACE,
         COMMAND_GET_TEXT,
         COMMAND_SET_TRACK_SELECTION_PARAMETERS,
@@ -1437,7 +1439,9 @@ public interface Player {
    *   <li>{@link #COMMAND_GET_DEVICE_VOLUME}
    *   <li>{@link #COMMAND_SET_VOLUME}
    *   <li>{@link #COMMAND_SET_DEVICE_VOLUME}
+   *   <li>{@link #COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS}
    *   <li>{@link #COMMAND_ADJUST_DEVICE_VOLUME}
+   *   <li>{@link #COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS}
    *   <li>{@link #COMMAND_SET_VIDEO_SURFACE}
    *   <li>{@link #COMMAND_GET_TEXT}
    *   <li>{@link #COMMAND_SET_TRACK_SELECTION_PARAMETERS}
@@ -1481,7 +1485,9 @@ public interface Player {
     COMMAND_GET_DEVICE_VOLUME,
     COMMAND_SET_VOLUME,
     COMMAND_SET_DEVICE_VOLUME,
+    COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS,
     COMMAND_ADJUST_DEVICE_VOLUME,
+    COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS,
     COMMAND_SET_VIDEO_SURFACE,
     COMMAND_GET_TEXT,
     COMMAND_SET_TRACK_SELECTION_PARAMETERS,
@@ -1788,27 +1794,36 @@ public interface Player {
    * #isCommandAvailable(int) available}.
    */
   int COMMAND_SET_VOLUME = 24;
-  /**
-   * Command to set the device volume.
-   *
-   * <p>The {@link #setDeviceVolume(int)} method must only be called if this command is {@linkplain
-   * #isCommandAvailable(int) available}.
-   */
-  int COMMAND_SET_DEVICE_VOLUME = 25;
 
   /**
-   * Command to increase and decrease the device volume and mute it.
+   * @deprecated Use {@link #COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS} instead.
+   */
+  @Deprecated int COMMAND_SET_DEVICE_VOLUME = 25;
+  /**
+   * Command to set the device volume with volume flags.
+   *
+   * <p>The {@link #setDeviceVolume(int, int)} method must only be called if this command is
+   * {@linkplain #isCommandAvailable(int) available}.
+   */
+  int COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS = 33;
+
+  /**
+   * @deprecated Use {@link #COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS} instead.
+   */
+  @Deprecated int COMMAND_ADJUST_DEVICE_VOLUME = 26;
+  /**
+   * Command to increase and decrease the device volume and mute it with volume flags.
    *
    * <p>The following methods must only be called if this command is {@linkplain
    * #isCommandAvailable(int) available}:
    *
    * <ul>
-   *   <li>{@link #increaseDeviceVolume()}
-   *   <li>{@link #decreaseDeviceVolume()}
-   *   <li>{@link #setDeviceMuted(boolean)}
+   *   <li>{@link #increaseDeviceVolume(int)}
+   *   <li>{@link #decreaseDeviceVolume(int)}
+   *   <li>{@link #setDeviceMuted(boolean, int)}
    * </ul>
    */
-  int COMMAND_ADJUST_DEVICE_VOLUME = 26;
+  int COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS = 34;
 
   /**
    * Command to set and clear the surface on which to render the video.
@@ -3095,36 +3110,68 @@ public interface Player {
   boolean isDeviceMuted();
 
   /**
-   * Sets the volume of the device.
+   * @deprecated Use {@link #setDeviceVolume(int, int)} instead.
+   */
+  @Deprecated
+  void setDeviceVolume(@IntRange(from = 0) int volume);
+
+  /**
+   * Sets the volume of the device with volume flags.
    *
-   * <p>This method must only be called if {@link #COMMAND_SET_DEVICE_VOLUME} is {@linkplain
-   * #getAvailableCommands() available}.
+   * <p>This method must only be called if {@link #COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS} is
+   * {@linkplain #getAvailableCommands() available}.
    *
    * @param volume The volume to set.
+   * @param flags Either 0 or a bitwise combination of one or more {@link C.VolumeFlags}.
    */
-  void setDeviceVolume(@IntRange(from = 0) int volume);
+  void setDeviceVolume(@IntRange(from = 0) int volume, int flags);
+
+  /**
+   * @deprecated Use {@link #increaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
+  void increaseDeviceVolume();
 
   /**
    * Increases the volume of the device.
    *
-   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME} is {@linkplain
-   * #getAvailableCommands() available}.
+   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS} is
+   * {@linkplain #getAvailableCommands() available}.
+   *
+   * @param flags Either 0 or a bitwise combination of one or more {@link C.VolumeFlags}.
    */
-  void increaseDeviceVolume();
+  void increaseDeviceVolume(@C.VolumeFlags int flags);
+
+  /**
+   * @deprecated Use {@link #decreaseDeviceVolume(int)} instead.
+   */
+  @Deprecated
+  void decreaseDeviceVolume();
 
   /**
    * Decreases the volume of the device.
    *
-   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME} is {@linkplain
-   * #getAvailableCommands() available}.
+   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS} is
+   * {@linkplain #getAvailableCommands() available}.
+   *
+   * @param flags Either 0 or a bitwise combination of one or more {@link C.VolumeFlags}.
    */
-  void decreaseDeviceVolume();
+  void decreaseDeviceVolume(@C.VolumeFlags int flags);
+
+  /**
+   * @deprecated Use {@link #setDeviceMuted(boolean, int)} instead.
+   */
+  @Deprecated
+  void setDeviceMuted(boolean muted);
 
   /**
    * Sets the mute state of the device.
    *
-   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME} is {@linkplain
-   * #getAvailableCommands() available}.
+   * <p>This method must only be called if {@link #COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS} is
+   * {@linkplain #getAvailableCommands() available}.
+   *
+   * @param muted Whether to set the device to be muted or not
+   * @param flags Either 0 or a bitwise combination of one or more {@link C.VolumeFlags}.
    */
-  void setDeviceMuted(boolean muted);
+  void setDeviceMuted(boolean muted, @C.VolumeFlags int flags);
 }

androidx/media

commit 136f0876d515b5b04731ac85f497123f6c270557

Fix `PlayerWrapper`'s creation of `VolumeProviderCompat`
When hardware buttons are used to control the volume of the remote device, the call propagates to `MediaSessionCompat.setPlaybackToRemote(volumeProviderCompat)`. However, `volumeProviderCompat` was created incorrectly when the new device volume commands were present (COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS and COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS), i.e. with volumeControlType = `VOLUME_CONTROL_FIXED`. This resulted in `VolumeProviderCompat` which doesn't call `onSetVolumeTo` or `onAdjustVolume` and hence doesn't propagate the calls to the `Player`. Instead, it only worked with the deprecated commands which ensured the volumeControlType was `VOLUME_CONTROL_ABSOLUTE`.

This bug was introduced in https://github.com/androidx/media/commit/c71e4bf1ffd4a0a0c631c27e4faa9480700e2c5f (1.0 media 3 release) when `PlayerWrapper`'s call to `createVolumeProviderCompat` was mostly rewritten to handle the new commands, but the two if-statements were not amended. Note: this change fixes the bug only for Android 11 and below. For 12 and above, there is a tracking bug for the regression that was introduced: https://issuetracker.google.com/issues/201546605

http://Issue: androidx/media#554

PiperOrigin-RevId: 554966361
(cherry picked from commit dedccc596eb75f9d3fcf7114e8541f3119a88382)

libraries/session/src/main/java/androidx/media3/session/PlayerWrapper.java
@@ -1031,9 +1031,11 @@ import java.util.List;
     }
     Commands availableCommands = getAvailableCommands();
     int volumeControlType = VolumeProviderCompat.VOLUME_CONTROL_FIXED;
-    if (availableCommands.contains(COMMAND_ADJUST_DEVICE_VOLUME)) {
+    if (availableCommands.containsAny(
+        COMMAND_ADJUST_DEVICE_VOLUME, COMMAND_ADJUST_DEVICE_VOLUME_WITH_FLAGS)) {
       volumeControlType = VolumeProviderCompat.VOLUME_CONTROL_RELATIVE;
-      if (availableCommands.contains(COMMAND_SET_DEVICE_VOLUME)) {
+      if (availableCommands.containsAny(
+          COMMAND_SET_DEVICE_VOLUME, COMMAND_SET_DEVICE_VOLUME_WITH_FLAGS)) {
         volumeControlType = VolumeProviderCompat.VOLUME_CONTROL_ABSOLUTE;
       }
     }