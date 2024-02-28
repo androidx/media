1e47553ff2149cd4319d8e01bb10fdad523a7dee
Steve Mayhew <mayhew.steve@gmail.com>
2024-02-27 16:42:50 -0800

    Updated with pull review comments
    Addressed comments from pull request:
    
    1. Converted `KeyLoadInfo` to builder pattern, made immutable
    2. Added `SchemaData` for the loaded key to the info
    3. Added method `MediaDrmCallback.getLastLoadEventInfo()`, allows the called object
       to create a true to spec `LoadEventInfo` object

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/util/EventLogger.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/util/EventLogger.java
@@ -41,6 +41,7 @@ import androidx.media3.exoplayer.DecoderReuseEvaluation;
 import androidx.media3.exoplayer.analytics.AnalyticsListener;
 import androidx.media3.exoplayer.audio.AudioSink;
 import androidx.media3.exoplayer.drm.DrmSession;
+import androidx.media3.exoplayer.drm.KeyLoadInfo;
 import androidx.media3.exoplayer.source.LoadEventInfo;
 import androidx.media3.exoplayer.source.MediaLoadData;
 import androidx.media3.exoplayer.trackselection.MappingTrackSelector;
@@ -516,7 +517,7 @@ public class EventLogger implements AnalyticsListener {
 
   @UnstableApi
   @Override
-  public void onDrmKeysLoaded(EventTime eventTime) {
+  public void onDrmKeysLoaded(EventTime eventTime, @Nullable KeyLoadInfo info) {
     logd(eventTime, "drmKeysLoaded");
   }
 
