3bb6cf212957391fddf26c977060300bb0b8ade0
ibaker <ibaker@google.com>
2024-05-28 04:41:08 -0700

    Remove unused `Util.getCommaDelimitedSimpleClassNames` method
    PiperOrigin-RevId: 637854422

--- libraries/common/src/main/java/androidx/media3/common/util/Util.java
+++ libraries/common/src/main/java/androidx/media3/common/util/Util.java
@@ -2047,24 +2047,6 @@ public final class Util {
   }
 
   /**
-   * Returns a string with comma delimited simple names of each object's class.
-   *
-   * @param objects The objects whose simple class names should be comma delimited and returned.
-   * @return A string with comma delimited simple names of each object's class.
-   */
-  @UnstableApi
-  public static String getCommaDelimitedSimpleClassNames(Object[] objects) {
-    StringBuilder stringBuilder = new StringBuilder();
-    for (int i = 0; i < objects.length; i++) {
-      stringBuilder.append(objects[i].getClass().getSimpleName());
-      if (i < objects.length - 1) {
-        stringBuilder.append(", ");
-      }
-    }
-    return stringBuilder.toString();
-  }
-
-  /**
    * Returns a user agent string based on the given application name and the library version.
    *
    * @param context A valid context of the calling application.
