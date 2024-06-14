4109e2edd9160832ce029a99276a39c60bf4c6b7
samrobinson <samrobinson@google.com>
2024-06-14 08:01:06 -0700

    Add more cases for parameterized video android tests.
    PiperOrigin-RevId: 643346610

--- libraries/transformer/src/androidTest/java/androidx/media3/transformer/ParameterizedInputSequenceExportTest.java
+++ libraries/transformer/src/androidTest/java/androidx/media3/transformer/ParameterizedInputSequenceExportTest.java
@@ -53,6 +53,8 @@ import org.junit.runners.Parameterized.Parameters;
 // TODO: b/343362776 - Add tests to assert enough silence is generated.
 // TODO: b/346289922 - Consider checking frame counts with extractors.
 // TODO: b/345483531 - Add support for asserting on duration for image only sequences.
+// TODO: b/345483531 - Split single item export tests into a separate class.
+// TODO: b/345483531 - Generate all permutations of all combinations of input files.
 
 /** Parameterized end to end {@linkplain EditedMediaItemSequence sequence} export tests. */
 @RunWith(Parameterized.class)
@@ -69,8 +71,26 @@ public class ParameterizedInputSequenceExportTest {
   @Parameters(name = "{0}")
   public static ImmutableList<SequenceConfig> params() {
     return ImmutableList.of(
+        new SequenceConfig(PNG_ITEM),
+        new SequenceConfig(PNG_ITEM, PNG_ITEM),
+        new SequenceConfig(PNG_ITEM, JPG_ITEM),
+        new SequenceConfig(PNG_ITEM, BT601_ITEM),
         new SequenceConfig(PNG_ITEM, BT709_ITEM),
+        new SequenceConfig(JPG_ITEM),
+        new SequenceConfig(JPG_ITEM, PNG_ITEM),
+        new SequenceConfig(JPG_ITEM, JPG_ITEM),
+        new SequenceConfig(JPG_ITEM, BT601_ITEM),
+        new SequenceConfig(JPG_ITEM, BT709_ITEM),
+        new SequenceConfig(BT601_ITEM),
+        new SequenceConfig(BT601_ITEM, PNG_ITEM),
+        new SequenceConfig(BT601_ITEM, JPG_ITEM),
+        new SequenceConfig(BT601_ITEM, BT601_ITEM),
+        new SequenceConfig(BT601_ITEM, BT709_ITEM),
+        new SequenceConfig(BT709_ITEM),
         new SequenceConfig(BT709_ITEM, PNG_ITEM),
+        new SequenceConfig(BT709_ITEM, JPG_ITEM),
+        new SequenceConfig(BT709_ITEM, BT601_ITEM),
+        new SequenceConfig(BT709_ITEM, BT709_ITEM),
         new SequenceConfig(
             BT709_ITEM, BT709_ITEM, PNG_ITEM, JPG_ITEM, BT709_ITEM, PNG_ITEM, BT709_ITEM),
         new SequenceConfig(
