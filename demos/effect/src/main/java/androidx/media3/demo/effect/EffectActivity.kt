7b9cfd196497340f1e8af963727967705e254002
shahddaghash <shahddaghash@google.com>
2024-11-06 09:15:44 -0800

    Add basic UI to effect demo app
    Added a basic UI to the effect demo app, including a PlayerView, buttons to choose preset input and choose local file, and a button to apply effects. The buttons are currently not implemented, and the app will show a snackbar message when they are clicked.
    
    PiperOrigin-RevId: 693751272

--- demos/effect/src/main/java/androidx/media3/demo/effect/EffectActivity.kt
+++ demos/effect/src/main/java/androidx/media3/demo/effect/EffectActivity.kt
@@ -20,28 +20,96 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.ui.PlayerView
+import kotlinx.coroutines.launch
 
 class EffectActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-          modifier = Modifier.fillMaxWidth(),
-          verticalArrangement = Arrangement.Center,
-          horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-          Text(text = "Effect demo", textAlign = TextAlign.Center)
-        }
+    setContent { EffectDemo() }
+  }
+
+  @Composable
+  fun EffectDemo() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+    ) { paddingValues ->
+      Column(
+        modifier = Modifier.fillMaxWidth().padding(paddingValues),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        InputChooser(
+          onButtonClick = {
+            coroutineScope.launch {
+              snackbarHostState.showSnackbar(message = "Button is not yet implemented.")
+            }
+          }
+        )
+        PlayerScreen()
+        Effects(
+          onButtonClick = {
+            coroutineScope.launch {
+              snackbarHostState.showSnackbar(message = "Button is not yet implemented.")
+            }
+          }
+        )
       }
     }
+  }
+
+  @Composable
+  fun InputChooser(onButtonClick: () -> Unit) {
+    Row(
+      modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.small_padding)),
+      horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.small_padding)),
+    ) {
+      Button(onClick = onButtonClick) {
+        Text(text = stringResource(id = R.string.choose_preset_input))
+      }
+      Button(onClick = onButtonClick) {
+        Text(text = stringResource(id = R.string.choose_local_file))
+      }
+    }
+  }
+
+  @Composable
+  fun PlayerScreen() {
+    val context = LocalContext.current
+    AndroidView(
+      factory = { PlayerView(context).apply {} },
+      modifier =
+        Modifier.height(dimensionResource(id = R.dimen.android_view_height))
+          .padding(all = dimensionResource(id = R.dimen.small_padding)),
+    )
+  }
+
+  @Composable
+  fun Effects(onButtonClick: () -> Unit) {
+    Button(onClick = onButtonClick) { Text(text = stringResource(id = R.string.apply_effects)) }
   }
 }
