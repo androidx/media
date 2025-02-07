3b5e1055412ff93227b5b01b2c01c38fcaacbc6c
Nevin Mital <nevmital@google.com>
2025-02-07 01:28:54 +0000

    Convert Composition sample to Kotlin

--- demos/composition/src/main/java/androidx/media3/demo/composition/MatrixTransformationFactory.kt
+++ demos/composition/src/main/java/androidx/media3/demo/composition/MatrixTransformationFactory.kt
@@ -13,81 +13,84 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package androidx.media3.demo.composition;
+package androidx.media3.demo.composition
 
-import android.graphics.Matrix;
-import androidx.media3.common.C;
-import androidx.media3.common.util.Util;
-import androidx.media3.effect.GlMatrixTransformation;
-import androidx.media3.effect.MatrixTransformation;
+import android.graphics.Matrix
+import androidx.media3.common.C
+import androidx.media3.common.util.Util
+import androidx.media3.effect.GlMatrixTransformation
+import androidx.media3.effect.MatrixTransformation
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
 /**
- * Factory for {@link GlMatrixTransformation GlMatrixTransformations} and {@link
- * MatrixTransformation MatrixTransformations} that create video effects by applying transformation
- * matrices to the individual video frames.
+ * Factory for [GlMatrixTransformations][GlMatrixTransformation] and
+ * [MatrixTransformations][MatrixTransformation] that create video effects by applying
+ * transformation matrices to the individual video frames.
  */
-/* package */ final class MatrixTransformationFactory {
-  /**
-   * Returns a {@link MatrixTransformation} that rescales the frames over the first {@link
-   * #ZOOM_DURATION_SECONDS} seconds, such that the rectangle filled with the input frame increases
-   * linearly in size from a single point to filling the full output frame.
-   */
-  public static MatrixTransformation createZoomInTransition() {
-    return MatrixTransformationFactory::calculateZoomInTransitionMatrix;
-  }
+internal object MatrixTransformationFactory {
+    /**
+     * Returns a [MatrixTransformation] that rescales the frames over the first
+     * [ZOOM_DURATION_SECONDS] seconds, such that the rectangle filled with the input frame
+     * increases linearly in size from a single point to filling the full output frame.
+     */
+    fun createZoomInTransition(): MatrixTransformation =
+        MatrixTransformation(::calculateZoomInTransitionMatrix)
 
-  /**
-   * Returns a {@link MatrixTransformation} that crops frames to a rectangle that moves on an
-   * ellipse.
-   */
-  public static MatrixTransformation createDizzyCropEffect() {
-    return MatrixTransformationFactory::calculateDizzyCropMatrix;
-  }
+    /**
+     * Returns a [MatrixTransformation] that crops frames to a rectangle that moves on an
+     * ellipse.
+     */
+    fun createDizzyCropEffect(): MatrixTransformation =
+        MatrixTransformation(::calculateDizzyCropMatrix)
 
-  /**
-   * Returns a {@link GlMatrixTransformation} that rotates a frame in 3D around the y-axis and
-   * applies perspective projection to 2D.
-   */
-  public static GlMatrixTransformation createSpin3dEffect() {
-    return MatrixTransformationFactory::calculate3dSpinMatrix;
-  }
+    /**
+     * Returns a [GlMatrixTransformation] that rotates a frame in 3D around the y-axis and
+     * applies perspective projection to 2D.
+     */
+    fun createSpin3dEffect(): GlMatrixTransformation =
+        GlMatrixTransformation(::calculate3dSpinMatrix)
 
-  private static final float ZOOM_DURATION_SECONDS = 2f;
-  private static final float DIZZY_CROP_ROTATION_PERIOD_US = 5_000_000f;
+    private const val ZOOM_DURATION_SECONDS = 2f
+    private const val DIZZY_CROP_ROTATION_PERIOD_US = 5000000f
 
-  private static Matrix calculateZoomInTransitionMatrix(long presentationTimeUs) {
-    Matrix transformationMatrix = new Matrix();
-    float scale = Math.min(1, presentationTimeUs / (C.MICROS_PER_SECOND * ZOOM_DURATION_SECONDS));
-    transformationMatrix.postScale(/* sx= */ scale, /* sy= */ scale);
-    return transformationMatrix;
-  }
+    private fun calculateZoomInTransitionMatrix(presentationTimeUs: Long): Matrix {
+        val transformationMatrix = Matrix()
+        val scale = min(1f, (presentationTimeUs / (C.MICROS_PER_SECOND * ZOOM_DURATION_SECONDS)))
+        transformationMatrix.postScale( /* sx= */scale,  /* sy= */scale)
+        return transformationMatrix
+    }
 
-  private static android.graphics.Matrix calculateDizzyCropMatrix(long presentationTimeUs) {
-    double theta = presentationTimeUs * 2 * Math.PI / DIZZY_CROP_ROTATION_PERIOD_US;
-    float centerX = 0.5f * (float) Math.cos(theta);
-    float centerY = 0.5f * (float) Math.sin(theta);
-    android.graphics.Matrix transformationMatrix = new android.graphics.Matrix();
-    transformationMatrix.postTranslate(/* dx= */ centerX, /* dy= */ centerY);
-    transformationMatrix.postScale(/* sx= */ 2f, /* sy= */ 2f);
-    return transformationMatrix;
-  }
+    private fun calculateDizzyCropMatrix(presentationTimeUs: Long): Matrix {
+        val theta = presentationTimeUs * 2f * Math.PI.toFloat() / DIZZY_CROP_ROTATION_PERIOD_US
+        val centerX = 0.5f * cos(theta)
+        val centerY = 0.5f * sin(theta)
+        val transformationMatrix = Matrix()
+        transformationMatrix.postTranslate( /* dx= */centerX,  /* dy= */centerY)
+        transformationMatrix.postScale( /* sx= */2f,  /* sy= */2f)
+        return transformationMatrix
+    }
 
-  private static float[] calculate3dSpinMatrix(long presentationTimeUs) {
-    float[] transformationMatrix = new float[16];
-    android.opengl.Matrix.frustumM(
-        transformationMatrix,
-        /* offset= */ 0,
-        /* left= */ -1f,
-        /* right= */ 1f,
-        /* bottom= */ -1f,
-        /* top= */ 1f,
-        /* near= */ 3f,
-        /* far= */ 5f);
-    android.opengl.Matrix.translateM(
-        transformationMatrix, /* mOffset= */ 0, /* x= */ 0f, /* y= */ 0f, /* z= */ -4f);
-    float theta = Util.usToMs(presentationTimeUs) / 10f;
-    android.opengl.Matrix.rotateM(
-        transformationMatrix, /* mOffset= */ 0, theta, /* x= */ 0f, /* y= */ 1f, /* z= */ 0f);
-    return transformationMatrix;
-  }
+    private fun calculate3dSpinMatrix(presentationTimeUs: Long): FloatArray {
+        val transformationMatrix = FloatArray(16)
+        android.opengl.Matrix.frustumM(
+            transformationMatrix,
+            /* offset= */0,
+            /* left= */-1f,
+            /* right= */1f,
+            /* bottom= */-1f,
+            /* top= */1f,
+            /* near= */3f,
+            /* far= */5f
+        )
+        android.opengl.Matrix.translateM(
+            transformationMatrix,  /* mOffset= */0,  /* x= */0f,  /* y= */0f,  /* z= */-4f
+        )
+        val theta = Util.usToMs(presentationTimeUs) / 10f
+        android.opengl.Matrix.rotateM(
+            transformationMatrix,  /* mOffset= */0, theta,  /* x= */0f,  /* y= */1f,  /* z= */0f
+        )
+        return transformationMatrix
+    }
 }
