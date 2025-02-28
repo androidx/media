8ea49025aaf14c7e7d953df8ca2f08a76d9d4275
Googler <noreply@google.com>
2025-02-28 10:41:16 -0800

    `FragmentedMp4Muxer`: Update `FragmentedMp4MuxerEndToEndTest`
    Update Test to test all supported mimeTypes in `FragmentedMp4Muxer`.
    
    PiperOrigin-RevId: 732187238

--- libraries/muxer/src/test/java/androidx/media3/muxer/FragmentedMp4MuxerEndToEndTest.java
+++ libraries/muxer/src/test/java/androidx/media3/muxer/FragmentedMp4MuxerEndToEndTest.java
@@ -51,15 +51,52 @@ import org.robolectric.ParameterizedRobolectricTestRunner.Parameters;
 /** End to end instrumentation tests for {@link FragmentedMp4Muxer}. */
 @RunWith(ParameterizedRobolectricTestRunner.class)
 public class FragmentedMp4MuxerEndToEndTest {
+  // Video Codecs
+  private static final String H263_3GP = "bbb_176x144_128kbps_15fps_h263.3gp";
   private static final String H264_MP4 = "sample_no_bframes.mp4";
+  private static final String H264_WITH_NON_REFERENCE_B_FRAMES_MP4 =
+      "bbb_800x640_768kbps_30fps_avc_non_reference_3b.mp4";
   private static final String H264_WITH_PYRAMID_B_FRAMES_MP4 =
       "bbb_800x640_768kbps_30fps_avc_pyramid_3b.mp4";
+  private static final String H264_WITH_FIRST_PTS_10_SEC =
+      "bbb_800x640_768kbps_30fps_avc_2b_firstpts_10_sec.mp4";
   private static final String H265_HDR10_MP4 = "hdr10-720p.mp4";
+  private static final String H265_WITH_METADATA_TRACK_MP4 = "h265_with_metadata_track.mp4";
+  private static final String APV_MP4 = "sample_with_apvc.mp4";
+  private static final String AV1_MP4 = "sample_av1.mp4";
+  private static final String MPEG4_MP4 = "bbb_176x144_192kbps_15fps_mpeg4.mp4";
+
+  // Contains CSD in CodecPrivate format.
+  private static final String VP9_MP4 = "bbb_800x640_768kbps_30fps_vp9.mp4";
+  private static final String VP9_WEB = "bbb_642x642_768kbps_30fps_vp9.webm";
+  // Audio Codecs
   private static final String AUDIO_ONLY_MP4 = "sample_audio_only_15s.mp4";
+  private static final String AMR_NB_3GP = "bbb_mono_8kHz_12.2kbps_amrnb.3gp";
+  private static final String AMR_WB_3GP = "bbb_mono_16kHz_23.05kbps_amrwb.3gp";
+  private static final String OPUS_OGG = "bbb_6ch_8kHz_opus.ogg";
+  private static final String VORBIS_OGG = "bbb_1ch_16kHz_q10_vorbis.ogg";
+  private static final String RAW_WAV = "bbb_2ch_44kHz.wav";
 
   @Parameters(name = "{0}")
   public static ImmutableList<String> mediaSamples() {
-    return ImmutableList.of(H264_MP4, H264_WITH_PYRAMID_B_FRAMES_MP4, H265_HDR10_MP4);
+    return ImmutableList.of(
+        H263_3GP,
+        H264_MP4,
+        H264_WITH_NON_REFERENCE_B_FRAMES_MP4,
+        H264_WITH_PYRAMID_B_FRAMES_MP4,
+        H264_WITH_FIRST_PTS_10_SEC,
+        H265_HDR10_MP4,
+        H265_WITH_METADATA_TRACK_MP4,
+        APV_MP4,
+        AV1_MP4,
+        MPEG4_MP4,
+        VP9_MP4,
+        VP9_WEB,
+        AMR_NB_3GP,
+        AMR_WB_3GP,
+        OPUS_OGG,
+        VORBIS_OGG,
+        RAW_WAV);
   }
 
   @Rule public final TemporaryFolder temporaryFolder = new TemporaryFolder();
