955d702c2e6fd90bcf52bd77307232470894df3d
Tomáš Kotula <Tomas.Kotula.con@hotstar.com>
2025-06-02 11:44:48 +0200

    Ads on multi period content

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/ads/AdsMediaSource.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/ads/AdsMediaSource.java
@@ -390,7 +390,7 @@ public final class AdsMediaSource extends CompositeMediaSource<MediaPeriodId> {
           .handleSourceInfoRefresh(newTimeline);
       maybeUpdateSourceInfo();
     } else {
-      checkArgument(newTimeline.getPeriodCount() == 1);
+//      checkArgument(newTimeline.getPeriodCount() == 1);
       contentTimeline = newTimeline;
       mainHandler.post(
           () -> {
@@ -541,7 +541,11 @@ public final class AdsMediaSource extends CompositeMediaSource<MediaPeriodId> {
         refreshSourceInfo(contentTimeline);
       } else {
         adPlaybackState = adPlaybackState.withAdDurationsUs(getAdDurationsUs());
-        refreshSourceInfo(new SinglePeriodAdTimeline(contentTimeline, adPlaybackState));
+        if (contentTimeline.getPeriodCount() == 1) {
+          refreshSourceInfo(new SinglePeriodAdTimeline(contentTimeline, adPlaybackState));
+        } else {
+          refreshSourceInfo(new MultiPeriodAdTimeline(contentTimeline, adPlaybackState));
+        }
       }
     }
   }
