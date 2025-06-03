00ca20bea4885c0742b0ac9d4c47acebe620078d
nift4 <nift4@protonmail.com>
2025-06-03 13:17:54 +0200

    Add PCM_DOUBLE and DSD format
    - PCM_DOUBLE is useful for signal processing, as it can contain both
      int32 and float32 losslessy.
    - Add DSD format for parity with platform.

--- libraries/common/src/main/java/androidx/media3/common/util/Util.java
+++ libraries/common/src/main/java/androidx/media3/common/util/Util.java
@@ -1190,6 +1190,19 @@ public final class Util {
   }
 
   /**
+   * Constrains a value to the specified bounds.
+   *
+   * @param value The value to constrain.
+   * @param min The lower bound.
+   * @param max The upper bound.
+   * @return The constrained value {@code Math.max(min, Math.min(value, max))}.
+   */
+  @UnstableApi
+  public static double constrainValue(double value, double min, double max) {
+    return max(min, min(value, max));
+  }
+
+  /**
    * Returns the sum of two arguments, or a third argument if the result overflows.
    *
    * @param x The first value.
@@ -2279,7 +2292,8 @@ public final class Util {
         || encoding == C.ENCODING_PCM_24BIT_BIG_ENDIAN
         || encoding == C.ENCODING_PCM_32BIT
         || encoding == C.ENCODING_PCM_32BIT_BIG_ENDIAN
-        || encoding == C.ENCODING_PCM_FLOAT;
+        || encoding == C.ENCODING_PCM_FLOAT
+        || encoding == C.ENCODING_PCM_DOUBLE;
   }
 
   /**
@@ -2294,7 +2308,8 @@ public final class Util {
         || encoding == C.ENCODING_PCM_24BIT_BIG_ENDIAN
         || encoding == C.ENCODING_PCM_32BIT
         || encoding == C.ENCODING_PCM_32BIT_BIG_ENDIAN
-        || encoding == C.ENCODING_PCM_FLOAT;
+        || encoding == C.ENCODING_PCM_FLOAT
+        || encoding == C.ENCODING_PCM_DOUBLE;
   }
 
   /**
@@ -2404,6 +2419,7 @@ public final class Util {
       case C.ENCODING_PCM_32BIT:
         return 31;
       case C.ENCODING_DTS_UHD_P2:
+      case C.ENCODING_DSD:
         return 34;
       default:
         return Integer.MAX_VALUE;
@@ -2443,6 +2459,8 @@ public final class Util {
       case C.ENCODING_PCM_32BIT_BIG_ENDIAN:
       case C.ENCODING_PCM_FLOAT:
         return 4;
+      case C.ENCODING_PCM_DOUBLE:
+        return 8;
       case C.ENCODING_INVALID:
       case Format.NO_VALUE:
       default:
