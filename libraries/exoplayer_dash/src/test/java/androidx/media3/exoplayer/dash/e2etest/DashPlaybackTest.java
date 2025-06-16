androidx/media

commit 49dec5db8b1b220f7c9a3e805e0087d775239a0b

Ignore renderer errors from text/metadata tracks
Before this change:

* With legacy subtitle decoding (at render time), load errors (e.g. HTTP
  404) would result playback completely failing, while parse errors
  (e.g. invalid  WebVTT data) would be silently ignored, so playback
  would continue without subtitles.
* With new subtitle decoding (at extraction time), both load and parse
  errors would result in playback completely failing.

This change means that now neither load nor parse errors in text or
metadata tracks stop playback from continuing. Instead the error'd track
is disabled until the end of the current period.

With new subtitle decoding, both load and parse errors happen during
loading/extraction, and so are emitted to the app via
`MediaSourceEventListener.onLoadError` and
`AnalyticsListener.onLoadError`. With legacy subtitle decoding, only
load errors are emitted via these listeners and parsing errors continue
to be silently ignored.

Issue: androidx/media#1722
PiperOrigin-RevId: 686902979

libraries/exoplayer/src/main/java/androidx/media3/exoplayer/text/TextRenderer.java
@@ -42,11 +42,13 @@ import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.exoplayer.source.SampleStream.ReadDataResult;
 import androidx.media3.extractor.text.CueDecoder;
 import androidx.media3.extractor.text.CuesWithTiming;
+import androidx.media3.extractor.text.Subtitle;
 import androidx.media3.extractor.text.SubtitleDecoder;
 import androidx.media3.extractor.text.SubtitleDecoderException;
 import androidx.media3.extractor.text.SubtitleInputBuffer;
 import androidx.media3.extractor.text.SubtitleOutputBuffer;
 import com.google.common.collect.ImmutableList;
+import java.io.IOException;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -122,6 +124,7 @@ public final class TextRenderer extends BaseRenderer implements Callback {
   private long lastRendererPositionUs;
   private long finalStreamEndPositionUs;
   private boolean legacyDecodingEnabled;
+  @Nullable private IOException streamError;
 
   /**
    * @param output The output.
@@ -472,9 +475,38 @@ public final class TextRenderer extends BaseRenderer implements Callback {
 
   @Override
   public boolean isReady() {
+    if (streamFormat == null) {
+      return true;
+    }
+    if (streamError == null) {
+      try {
+        maybeThrowStreamError();
+      } catch (IOException e) {
+        streamError = e;
+      }
+    }
+
+    if (streamError != null) {
+      if (isCuesWithTiming(checkNotNull(streamFormat))) {
+        return checkNotNull(cuesResolver).getNextCueChangeTimeUs(lastRendererPositionUs)
+            != C.TIME_END_OF_SOURCE;
+      } else {
+        if (outputStreamEnded
+            || (inputStreamEnded
+                && hasNoEventsAfter(subtitle, lastRendererPositionUs)
+                && hasNoEventsAfter(nextSubtitle, lastRendererPositionUs)
+                && nextSubtitleInputBuffer != null)) {
+          return false;
+        }
+      }
+    }
     // Don't block playback whilst subtitles are loading.
     // Note: To change this behavior, it will be necessary to consider [Internal: b/12949941].
     return true;
+  }
+
+  private static boolean hasNoEventsAfter(@Nullable Subtitle subtitle, long timeUs) {
+    return subtitle == null || subtitle.getEventTime(subtitle.getEventTimeCount() - 1) <= timeUs;
   }
 
   private void releaseSubtitleBuffers() {

commit baffe5d48b28d349d40aa17c255957dca8ee1941

`TextRenderer`: Only check for a stream error when there are no cues
This is a more complete fix for the inconsistent state introduced in
https://github.com/androidx/media/commit/49dec5db8b1b220f7c9a3e805e0087d775239a0b and partially fixed in https://github.com/androidx/media/commit/ecfd9d1e92ec12aba85139f14e91de51da3605da.

This change removes the `streamError` field completely, so there's no
need to clear it in other methods, and it only checks for an error
when we have no future cue change events.

Issue: androidx/media#2517

The test data in this change was created by:

1. Modifying `standalone-webvtt/sample.vtt` to have a subtitle in the 1st, 2nd and 5th seconds (so that the 3rd and 4th segments will be empty).
2. Re-encoding `preroll-5s.mp4` to have an I-frame every 1s
3. Creating the DASH stream with shaka packager:

   ```shell
   $ packager \
      'in=../5s.mp4,stream=video,segment_template=video$Number$.m4s,init_segment=video_init.mp4' \
      'in=../standalone-webvtt/sample.vtt,stream=text,segment_template=text$Number$.m4s,init_segment=text_init.mp4' \
      --mpd_output manifest.mpd \
      --segment_duration=1 \
      --generate_static_live_mpd
   ```

PiperOrigin-RevId: 771916959

libraries/exoplayer/src/main/java/androidx/media3/exoplayer/text/TextRenderer.java
@@ -124,7 +124,6 @@ public final class TextRenderer extends BaseRenderer implements Callback {
   private long lastRendererPositionUs;
   private long finalStreamEndPositionUs;
   private boolean legacyDecodingEnabled;
-  @Nullable private IOException streamError;
 
   /**
    * @param output The output.
@@ -208,7 +207,6 @@ public final class TextRenderer extends BaseRenderer implements Callback {
       long offsetUs,
       MediaSource.MediaPeriodId mediaPeriodId) {
     streamFormat = formats[0];
-    streamError = null;
     if (!isCuesWithTiming(streamFormat)) {
       assertLegacyDecodingEnabledIfRequired();
       if (subtitleDecoder != null) {
@@ -461,7 +459,6 @@ public final class TextRenderer extends BaseRenderer implements Callback {
   @Override
   protected void onDisabled() {
     streamFormat = null;
-    streamError = null;
     finalStreamEndPositionUs = C.TIME_UNSET;
     clearOutput();
     lastRendererPositionUs = C.TIME_UNSET;
@@ -480,35 +477,35 @@ public final class TextRenderer extends BaseRenderer implements Callback {
     if (streamFormat == null) {
       return true;
     }
-    if (streamError == null) {
-      try {
-        maybeThrowStreamError();
-      } catch (IOException e) {
-        streamError = e;
-      }
-    }
 
-    if (streamError != null) {
-      if (isCuesWithTiming(checkNotNull(streamFormat))) {
-        return checkNotNull(cuesResolver).getNextCueChangeTimeUs(lastRendererPositionUs)
-            != C.TIME_END_OF_SOURCE;
+    // We don't block playback whilst subtitles are loading.
+    // Note: To change this behavior, it will be necessary to consider [Internal: b/12949941].
+    if (isCuesWithTiming(checkNotNull(streamFormat))) {
+      if (checkNotNull(cuesResolver).getNextCueChangeTimeUs(lastRendererPositionUs)
+          != C.TIME_END_OF_SOURCE) {
+        // We have a cue change loaded in the future.
+        return true;
       } else {
-        if (outputStreamEnded
-            || (inputStreamEnded
-                && hasNoEventsAfter(subtitle, lastRendererPositionUs)
-                && hasNoEventsAfter(nextSubtitle, lastRendererPositionUs)
-                && nextSubtitleInputBuffer != null)) {
+        // We don't have any future cues, so let's see if there's a loading error, and return
+        // ready=false if so.
+        try {
+          maybeThrowStreamError();
+          return true;
+        } catch (IOException e) {
           return false;
         }
       }
+    } else {
+      return !outputStreamEnded
+          && (!inputStreamEnded
+              || hasEventsAfter(subtitle, lastRendererPositionUs)
+              || hasEventsAfter(nextSubtitle, lastRendererPositionUs)
+              || nextSubtitleInputBuffer == null);
     }
-    // Don't block playback whilst subtitles are loading.
-    // Note: To change this behavior, it will be necessary to consider [Internal: b/12949941].
-    return true;
   }
 
-  private static boolean hasNoEventsAfter(@Nullable Subtitle subtitle, long timeUs) {
-    return subtitle == null || subtitle.getEventTime(subtitle.getEventTimeCount() - 1) <= timeUs;
+  private static boolean hasEventsAfter(@Nullable Subtitle subtitle, long timeUs) {
+    return subtitle != null && subtitle.getEventTime(subtitle.getEventTimeCount() - 1) > timeUs;
   }
 
   private void releaseSubtitleBuffers() {

libraries/test_utils/build.gradle
@@ -35,10 +35,10 @@ dependencies {
     api 'junit:junit:' + junitVersion
     api 'com.google.truth:truth:' + truthVersion
     api 'com.google.truth.extensions:truth-java8-extension:' + truthVersion
+    api 'com.squareup.okhttp3:mockwebserver:' + okhttpVersion
     compileOnly 'org.checkerframework:checker-qual:' + checkerframeworkVersion
     compileOnly 'org.jetbrains.kotlin:kotlin-annotations-jvm:' + kotlinAnnotationsVersion
     implementation 'androidx.annotation:annotation:' + androidxAnnotationVersion
-    implementation 'com.squareup.okhttp3:mockwebserver:' + okhttpVersion
     api project(modulePrefix + 'lib-exoplayer')
     api project(modulePrefix + 'lib-transformer')
     testImplementation project(modulePrefix + 'test-utils-robolectric')

libraries/exoplayer_dash/src/test/java/androidx/media3/exoplayer/dash/e2etest/DashPlaybackTest.java
@@ -16,6 +16,8 @@
 package androidx.media3.exoplayer.dash.e2etest;
 
 import static androidx.media3.common.util.Assertions.checkNotNull;
+import static androidx.media3.test.utils.WebServerDispatcher.NOT_FOUND_BODY;
+import static androidx.media3.test.utils.WebServerDispatcher.getRequestPath;
 import static androidx.media3.test.utils.robolectric.TestPlayerRunHelper.advance;
 import static androidx.media3.test.utils.robolectric.TestPlayerRunHelper.play;
 import static com.google.common.truth.Truth.assertThat;
@@ -27,6 +29,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.SurfaceTexture;
 import android.net.Uri;
 import android.view.Surface;
@@ -35,7 +38,11 @@ import androidx.media3.common.C;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.ParserException;
 import androidx.media3.common.Player;
+import androidx.media3.common.util.ConditionVariable;
+import androidx.media3.datasource.AssetDataSource;
 import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DataSourceUtil;
+import androidx.media3.datasource.DataSpec;
 import androidx.media3.datasource.DefaultDataSource;
 import androidx.media3.datasource.ResolvingDataSource;
 import androidx.media3.exoplayer.DecoderCounters;
@@ -51,23 +58,33 @@ import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
 import androidx.media3.exoplayer.source.LoadEventInfo;
 import androidx.media3.exoplayer.source.MediaLoadData;
 import androidx.media3.exoplayer.source.chunk.BundledChunkExtractor;
+import androidx.media3.exoplayer.text.TextRenderer;
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
 import androidx.media3.exoplayer.upstream.CmcdConfiguration;
 import androidx.media3.test.utils.CapturingRenderersFactory;
 import androidx.media3.test.utils.DumpFileAsserts;
 import androidx.media3.test.utils.FakeClock;
 import androidx.media3.test.utils.ThrowingSubtitleParserFactory;
+import androidx.media3.test.utils.WebServerDispatcher;
 import androidx.media3.test.utils.robolectric.PlaybackOutput;
 import androidx.media3.test.utils.robolectric.ShadowMediaCodecConfig;
 import androidx.media3.test.utils.robolectric.TestPlayerRunHelper;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.robolectric.annotation.Config;
@@ -79,6 +96,8 @@ public final class DashPlaybackTest {
   @Rule
   public ShadowMediaCodecConfig mediaCodecConfig =
       ShadowMediaCodecConfig.withAllDefaultSupportedCodecs();
+
+  @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
   @Test
   public void webvttStandaloneFile() throws Exception {
@@ -255,6 +274,110 @@ public final class DashPlaybackTest {
 
     DumpFileAsserts.assertOutput(
         applicationContext, playbackOutput, "playbackdumps/dash/webvtt-in-mp4.dump");
+  }
+
+  /**
+   * Regression test for <a href="https://github.com/androidx/media/issues/2517">issue #2517</a>.
+   *
+   * <p>The test DASH manifest contains 5 1s segments of video and text data. The first, second and
+   * fifth text segments have subtitle data while the third and fourth are empty.
+   *
+   * <p>The test returns a 404 response for the second text segment until this failure is handled by
+   * {@link TextRenderer}, at which point it starts resolving but blocks loading the fifth segment.
+   * This ensures {@link TextRenderer} sees an error, and {@link TextRenderer#isReady()} is called
+   * with no future subtitle data when entering the third segment. Without the fix, the test hangs
+   * at this transition.
+   */
+  @Test
+  public void webvttInMp4_transientLoadError_playbackContinues() throws Exception {
+    MockWebServer mockWebServer = new MockWebServer();
+    WebServerDispatcher webServerDispatcher =
+        WebServerDispatcher.forResources(
+            mockWebServerResourcesFromAssetsDirectory(
+                "media/dash/webvtt-in-mp4-multiple-segments"));
+    AtomicInteger secondSubtitleFailureCount = new AtomicInteger();
+    AtomicBoolean secondSubtitleResolves = new AtomicBoolean();
+    ConditionVariable fifthSubtitleLoad = new ConditionVariable();
+    Dispatcher blockingDispatcher =
+        new Dispatcher() {
+          @Override
+          public MockResponse dispatch(RecordedRequest recordedRequest)
+              throws InterruptedException {
+            if (getRequestPath(recordedRequest).endsWith("text5.m4s")) {
+              fifthSubtitleLoad.block();
+            }
+            if (getRequestPath(recordedRequest).endsWith("text2.m4s")
+                && !secondSubtitleResolves.get()) {
+              secondSubtitleFailureCount.incrementAndGet();
+              return new MockResponse().setBody(NOT_FOUND_BODY).setResponseCode(404);
+            }
+            return webServerDispatcher.dispatch(recordedRequest);
+          }
+        };
+    mockWebServer.setDispatcher(blockingDispatcher);
+
+    Context applicationContext = ApplicationProvider.getApplicationContext();
+    ExoPlayer player =
+        new ExoPlayer.Builder(applicationContext)
+            .setClock(new FakeClock(/* isAutoAdvancing= */ true))
+            .build();
+    Surface surface = new Surface(new SurfaceTexture(/* texName= */ 1));
+    player.setVideoSurface(surface);
+    AtomicBoolean secondSubtitleChunkLoaded = new AtomicBoolean();
+    player.addAnalyticsListener(
+        new AnalyticsListener() {
+          @Override
+          public void onLoadCompleted(
+              EventTime eventTime, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
+            if (loadEventInfo.dataSpec.uri.getPath().endsWith("text2.m4s")) {
+              secondSubtitleChunkLoaded.set(true);
+            }
+          }
+        });
+    // Ensure the subtitle track is selected.
+    DefaultTrackSelector trackSelector =
+        checkNotNull((DefaultTrackSelector) player.getTrackSelector());
+    trackSelector.setParameters(
+        trackSelector
+            .buildUponParameters()
+            .setPreferredTextLanguage("en")
+            .setSelectUndeterminedTextLanguage(true));
+    // Deliberately don't capture the video data, since we don't care about it for this test.
+    PlaybackOutput playbackOutput = PlaybackOutput.registerWithoutRendererCapture(player);
+
+    player.setMediaItem(MediaItem.fromUri(mockWebServer.url("manifest.mpd").toString()));
+    player.prepare();
+
+    // Ensure the loading error is processed by the player, and the segment is subsequently
+    // successfully loaded, before playback starts (otherwise in the test playback progresses too
+    // quickly, and completes (without any subtitles) before the load error is encountered). The
+    // 4 load errors allow for the default 3 retries, plus a final failure which propagates to
+    // TextRenderer.
+    advance(player)
+        .ignoringNonFatalErrors()
+        .untilBackgroundThreadCondition(() -> secondSubtitleFailureCount.get() == 4);
+    secondSubtitleResolves.set(true);
+    advance(player)
+        .ignoringNonFatalErrors()
+        .untilBackgroundThreadCondition(secondSubtitleChunkLoaded::get);
+    player.play();
+
+    // Progress past the transition into the third segment (which is the first empty one), then
+    // allow the fifth (non-empty) subtitle segment to load. This ensures we enter the third segment
+    // with no future subtitle data in TextRenderer.
+    advance(player)
+        .ignoringNonFatalErrors()
+        .untilPosition(/* mediaItemIndex= */ 0, /* positionMs= */ 3500);
+    fifthSubtitleLoad.open();
+    advance(player).ignoringNonFatalErrors().untilState(Player.STATE_ENDED);
+    player.release();
+    surface.release();
+    mockWebServer.close();
+
+    DumpFileAsserts.assertOutput(
+        applicationContext,
+        playbackOutput,
+        "playbackdumps/dash/webvtt-in-mp4-multiple-segments.textonly.dump");
   }
 
   @Test
@@ -823,6 +946,32 @@ public final class DashPlaybackTest {
     surface.release();
 
     verify(listener, never()).onIsLoadingChanged(true);
+  }
+
+  private static ImmutableList<WebServerDispatcher.Resource>
+      mockWebServerResourcesFromAssetsDirectory(String assetDirectory) throws IOException {
+    Context context = ApplicationProvider.getApplicationContext();
+    AssetManager assetManager = context.getAssets();
+    String[] contents = assetManager.list(assetDirectory);
+    AssetDataSource assetDataSource =
+        new AssetDataSource(ApplicationProvider.getApplicationContext());
+    ImmutableList.Builder<WebServerDispatcher.Resource> resources =
+        ImmutableList.builderWithExpectedSize(contents.length);
+    for (String asset : contents) {
+      try {
+        Uri assetUri =
+            new Uri.Builder().scheme("asset").appendPath(assetDirectory).appendPath(asset).build();
+        assetDataSource.open(new DataSpec(assetUri));
+        resources.add(
+            new WebServerDispatcher.Resource.Builder()
+                .setPath(asset)
+                .setData(DataSourceUtil.readToEnd(assetDataSource))
+                .build());
+      } finally {
+        assetDataSource.close();
+      }
+    }
+    return resources.build();
   }
 
   private static final class AnalyticsListenerImpl implements AnalyticsListener {

libraries/test_data/src/test/assets/media/dash/webvtt-in-mp4-multiple-segments/manifest.mpd
@@ -1,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--Generated with https://github.com/shaka-project/shaka-packager version v3.4.2-c819dea-release-->
+<MPD xmlns="urn:mpeg:dash:schema:mpd:2011" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:mpeg:dash:schema:mpd:2011 DASH-MPD.xsd" profiles="urn:mpeg:dash:profile:isoff-live:2011" minBufferTime="PT2S" type="static" mediaPresentationDuration="PT5S">
+  <Period id="0">
+    <AdaptationSet id="0" contentType="video" width="640" height="360" frameRate="12800/512" segmentAlignment="true" par="16:9">
+      <Representation id="0" bandwidth="44353" codecs="hev1.1.6.L63.90" mimeType="video/mp4" sar="1:1">
+        <SegmentTemplate timescale="12800" initialization="video_init.mp4" media="video$Number$.m4s" startNumber="1">
+          <SegmentTimeline>
+            <S t="0" d="10752"/>
+            <S t="10752" d="13312"/>
+            <S t="24064" d="12800" r="1"/>
+            <S t="49664" d="14336"/>
+          </SegmentTimeline>
+        </SegmentTemplate>
+      </Representation>
+    </AdaptationSet>
+    <AdaptationSet id="1" contentType="text" segmentAlignment="true">
+      <Role schemeIdUri="urn:mpeg:dash:role:2011" value="subtitle"/>
+      <Representation id="1" bandwidth="2296" codecs="wvtt" mimeType="application/mp4">
+        <SegmentTemplate timescale="1000" initialization="text_init.mp4" media="text$Number$.m4s" startNumber="1">
+          <SegmentTimeline>
+            <S t="0" d="1000" r="4"/>
+          </SegmentTimeline>
+        </SegmentTemplate>
+      </Representation>
+    </AdaptationSet>
+  </Period>
+</MPD>

libraries/test_data/src/test/assets/playbackdumps/dash/webvtt-in-mp4-multiple-segments.textonly.dump
@@ -1,0 +1,58 @@
+TextOutput:
+  Subtitle[0]:
+    presentationTimeUs = 0
+    Cues = []
+  Subtitle[1]:
+    presentationTimeUs = 0
+    Cues = []
+  Subtitle[2]:
+    presentationTimeUs = 20000
+    Cue[0]:
+      text = Subtitle in the first segment.
+      textAlignment = ALIGN_CENTER
+      lineType = 1
+      lineAnchor = 0
+      position = 0.5
+      positionAnchor = 1
+      size = 1.0
+  Subtitle[3]:
+    presentationTimeUs = 834000
+    Cues = []
+  Subtitle[4]:
+    presentationTimeUs = 1000000
+    Cues = []
+  Subtitle[5]:
+    presentationTimeUs = 1100000
+    Cue[0]:
+      text = Subtitle in the second segment.
+      textAlignment = ALIGN_CENTER
+      lineType = 1
+      lineAnchor = 0
+      position = 0.5
+      positionAnchor = 1
+      size = 1.0
+  Subtitle[6]:
+    presentationTimeUs = 1956000
+    Cues = []
+  Subtitle[7]:
+    presentationTimeUs = 2000000
+    Cues = []
+  Subtitle[8]:
+    presentationTimeUs = 3000000
+    Cues = []
+  Subtitle[9]:
+    presentationTimeUs = 4000000
+    Cues = []
+  Subtitle[10]:
+    presentationTimeUs = 4500000
+    Cue[0]:
+      text = Subtitle in the fifth segment.
+      textAlignment = ALIGN_CENTER
+      lineType = 1
+      lineAnchor = 0
+      position = 0.5
+      positionAnchor = 1
+      size = 1.0
+  Subtitle[11]:
+    presentationTimeUs = 4956000
+    Cues = []