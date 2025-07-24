04c85a908a71856d6c916c719dead6329182e5c4
nift4 <nift4@protonmail.com>
2025-07-24 18:18:14 +0200

    Move legacy session calls to a new thread
    Issue: https://github.com/androidx/media/issues/2577
    Test: add Thread.sleep(5000) to setMetadata(), app stays responsive

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
@@ -232,7 +232,6 @@ import org.checkerframework.checker.initialization.qual.Initialized;
         new MediaSessionLegacyStub(
             /* session= */ thisRef,
             sessionUri,
-            applicationHandler,
             tokenExtras,
             playIfSuppressed,
             customLayout,
@@ -242,7 +241,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
             sessionExtras,
             packageNameOverride);
 
-    Token platformToken = sessionLegacyStub.getSessionCompat().getSessionToken().getToken();
+    Token platformToken = sessionLegacyStub.getSessionToken().getToken();
     sessionToken =
         new SessionToken(
             Process.myUid(),
@@ -1014,7 +1013,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
 
   @SuppressWarnings("UnnecessarilyFullyQualified") // Avoiding confusion by just using "Token"
   public android.media.session.MediaSession.Token getPlatformToken() {
-    return sessionLegacyStub.getSessionCompat().getSessionToken().getToken();
+    return sessionLegacyStub.getSessionToken().getToken();
   }
 
   public void setLegacyControllerConnectionTimeoutMs(long timeoutMs) {
@@ -1088,8 +1087,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
     MediaSessionServiceLegacyStub legacyStub;
     synchronized (lock) {
       if (browserServiceLegacyStub == null) {
-        browserServiceLegacyStub =
-            createLegacyBrowserService(sessionLegacyStub.getSessionCompat().getSessionToken());
+        browserServiceLegacyStub = createLegacyBrowserService(sessionLegacyStub.getSessionToken());
       }
       legacyStub = browserServiceLegacyStub;
     }
@@ -1548,7 +1546,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
         sessionLegacyStub.onSkipToNext();
         return true;
       } else if (callerInfo.getControllerVersion() != ControllerInfo.LEGACY_CONTROLLER_VERSION) {
-        sessionLegacyStub.getSessionCompat().getController().dispatchMediaButtonEvent(keyEvent);
+        sessionLegacyStub.getControllerCompat().dispatchMediaButtonEvent(keyEvent);
         return true;
       }
       // This is an unhandled framework event. Return false to let the framework resolve by calling
