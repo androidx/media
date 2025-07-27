c6c0cf9ed85ac42cf5a71028f7b6f0f34012af6d
nift4 <nift4@protonmail.com>
2025-07-27 17:31:23 +0200

    Allow opting out of media button playback resumption at runtime
    This is a step towards Issue: https://github.com/androidx/media/issues/770

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
@@ -924,6 +924,12 @@ import org.checkerframework.checker.initialization.qual.Initialized;
     }
   }
 
+  public void setOptOutOfMediaButtonPlaybackResumption(
+      boolean optOutOfMediaButtonPlaybackResumption) {
+    sessionLegacyStub.setOptOutOfMediaButtonPlaybackResumption(
+        optOutOfMediaButtonPlaybackResumption);
+  }
+
   public void onPostConnectOnHandler(ControllerInfo controller) {
     if (isMediaNotificationControllerConnected && isSystemUiController(controller)) {
       // Hide System UI. Apps can use the media notification controller to maintain the platform
