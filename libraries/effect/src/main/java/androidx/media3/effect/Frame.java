8162319d92fc5e92b377f7e665f260f3eb0354a2
kggibson <kggibson@google.com>
2025-08-20 09:46:57 -0700

    Add Frame, FrameConsumer and FrameProcessorinterfaces
    PiperOrigin-RevId: 797352712

---
+++ libraries/effect/src/main/java/androidx/media3/effect/Frame.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.media3.effect;
+
+/**
+ * An interface that represents a single frame of data.
+ *
+ * <p>This interface is experimental and subject to change.
+ *
+ * <p>A {@code Frame} is the basic unit of data that {@link FrameConsumer}s can process.
+ * Implementations should wrap the underlying data objects, such as a {@link
+ * android.graphics.Bitmap} or a GL texture.
+ */
+/* package */ interface Frame {
+
+  /** A marker interface for storing arbitrary metadata associated with a {@link Frame}. */
+  interface Metadata {}
+
+  /** Returns the {@link Metadata} associated with this frame. */
+  Frame.Metadata getMetadata();
+
+  /** Releases the frame and its underlying resources. */
+  void release();
+}
