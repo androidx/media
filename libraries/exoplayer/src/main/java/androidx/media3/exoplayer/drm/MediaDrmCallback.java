darulkirom/M3ExoPlayer

Provides thread-safe usage of `LoadEventInfo` in `HttpMediaDrmCallback`
1. Restore creation of `LoadEventInfo` to the code that was migrated to `DrmUtil.executePost()`
   that was removed in the rebase.
2. Refactor change signature of `MediaDrmCallback` to return both the byte[] response and an
   assoicated `LoadEventInfo`

libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/MediaDrmCallback.java
@@ -15,7 +15,6 @@
  */
 package androidx.media3.exoplayer.drm;
 
-import androidx.annotation.Nullable;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.exoplayer.drm.ExoMediaDrm.KeyRequest;
 import androidx.media3.exoplayer.drm.ExoMediaDrm.ProvisionRequest;
@@ -27,14 +26,31 @@ import java.util.UUID;
 public interface MediaDrmCallback {
 
   /**
+   * Response data from the {@link MediaDrmCallback} requests.
+   *
+   * <p>Encapsulates the license server response data {@link #responseData} along with information
+   * ({@link #loadEventInfo} about the network transfer (if any) that was issued to gather the
+   * response.
+   */
+  public class KeyResponse {
+    public final byte[] responseData;
+    public final LoadEventInfo loadEventInfo;
+
+    public KeyResponse(byte[] responseData, LoadEventInfo loadEventInfo) {
+      this.responseData = responseData;
+      this.loadEventInfo = loadEventInfo;
+    }
+  }
+
+  /**
    * Executes a provisioning request.
    *
    * @param uuid The UUID of the content protection scheme.
    * @param request The request.
-   * @return The response data.
+   * @return A {@link KeyResponse} that holds the response payload, and LoadEventInfo
    * @throws MediaDrmCallbackException If an error occurred executing the request.
    */
-  byte[] executeProvisionRequest(UUID uuid, ProvisionRequest request)
+  KeyResponse executeProvisionRequest(UUID uuid, ProvisionRequest request)
       throws MediaDrmCallbackException;
 
   /**
@@ -42,23 +58,8 @@ public interface MediaDrmCallback {
    *
    * @param uuid The UUID of the content protection scheme.
    * @param request The request.
-   * @return The response data.
+   * @return A {@link KeyResponse} that holds the response payload, and LoadEventInfo
    * @throws MediaDrmCallbackException If an error occurred executing the request.
    */
-  byte[] executeKeyRequest(UUID uuid, KeyRequest request) throws MediaDrmCallbackException;
-
-  /**
-   * Get the {@link LoadEventInfo} for the last executed request.
-   *
-   * <p>Valid after a call to {@link #executeKeyRequest(UUID, KeyRequest)} or {@link
-   * #executeProvisionRequest(UUID, ProvisionRequest)}, either contains the load event info for that
-   * request or null if no load was performed or this implementation does not support reporting load
-   * info
-   *
-   * @return the {@link LoadEventInfo} or null if no load was performed
-   */
-  @Nullable
-  default LoadEventInfo getLastLoadEventInfo() {
-    return null;
-  }
+  KeyResponse executeKeyRequest(UUID uuid, KeyRequest request) throws MediaDrmCallbackException;
 }

libraries/exoplayer/src/test/java/androidx/media3/exoplayer/analytics/DefaultAnalyticsCollectorTest.java
@@ -74,6 +74,7 @@ import static org.robolectric.shadows.ShadowLooper.idleMainLooper;
 import static org.robolectric.shadows.ShadowLooper.runMainLooperToNextTask;
 
 import android.graphics.SurfaceTexture;
+import android.net.Uri;
 import android.os.Looper;
 import android.util.SparseArray;
 import android.view.Surface;
@@ -96,6 +97,7 @@ import androidx.media3.common.util.Clock;
 import androidx.media3.common.util.ConditionVariable;
 import androidx.media3.common.util.HandlerWrapper;
 import androidx.media3.common.util.Util;
+import androidx.media3.datasource.DataSpec;
 import androidx.media3.exoplayer.DecoderCounters;
 import androidx.media3.exoplayer.DecoderReuseEvaluation;
 import androidx.media3.exoplayer.ExoPlaybackException;
@@ -106,6 +108,8 @@ import androidx.media3.exoplayer.drm.DefaultDrmSessionManager;
 import androidx.media3.exoplayer.drm.DrmSession;
 import androidx.media3.exoplayer.drm.DrmSessionManager;
 import androidx.media3.exoplayer.drm.ExoMediaDrm;
+import androidx.media3.exoplayer.drm.ExoMediaDrm.KeyRequest;
+import androidx.media3.exoplayer.drm.ExoMediaDrm.ProvisionRequest;
 import androidx.media3.exoplayer.drm.MediaDrmCallback;
 import androidx.media3.exoplayer.drm.MediaDrmCallbackException;
 import androidx.media3.exoplayer.source.LoadEventInfo;
@@ -131,6 +135,7 @@ import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
@@ -190,6 +195,15 @@ public final class DefaultAnalyticsCollectorTest {
   private EventWindowAndPeriodId period1Seq2;
   private EventWindowAndPeriodId window0Period1Seq0;
   private EventWindowAndPeriodId window1Period0Seq1;
+  private static final LoadEventInfo FAKE_LOAD_EVENT_INFO =
+      new LoadEventInfo(
+          1,
+          new DataSpec.Builder().setUri(Uri.EMPTY).build(),
+          Uri.EMPTY,
+          Collections.emptyMap(),
+          1000,
+          2000,
+          8192);
 
   /**
    * Verify that {@link DefaultAnalyticsCollector} explicitly overrides all {@link Player.Listener}
@@ -2402,13 +2416,13 @@ public final class DefaultAnalyticsCollectorTest {
    */
   private static final class EmptyDrmCallback implements MediaDrmCallback {
     @Override
-    public byte[] executeProvisionRequest(UUID uuid, ExoMediaDrm.ProvisionRequest request) {
-      return new byte[0];
+    public KeyResponse executeProvisionRequest(UUID uuid, ProvisionRequest request) {
+      return new KeyResponse(new byte[0], FAKE_LOAD_EVENT_INFO);
     }
 
     @Override
-    public byte[] executeKeyRequest(UUID uuid, ExoMediaDrm.KeyRequest request) {
-      return new byte[0];
+    public KeyResponse executeKeyRequest(UUID uuid, KeyRequest request) {
+      return new KeyResponse(new byte[0], FAKE_LOAD_EVENT_INFO);
     }
   }
 
@@ -2442,26 +2456,26 @@ public final class DefaultAnalyticsCollectorTest {
     }
 
     @Override
-    public byte[] executeProvisionRequest(UUID uuid, ExoMediaDrm.ProvisionRequest request)
+    public KeyResponse executeProvisionRequest(UUID uuid, ProvisionRequest request)
         throws MediaDrmCallbackException {
       provisionCondition.blockUninterruptible();
       provisionCondition.close();
       if (alwaysFail) {
         throw new RuntimeException("executeProvisionRequest failed");
       } else {
-        return new byte[0];
+        return new KeyResponse(new byte[0], FAKE_LOAD_EVENT_INFO);
       }
     }
 
     @Override
-    public byte[] executeKeyRequest(UUID uuid, ExoMediaDrm.KeyRequest request)
+    public KeyResponse executeKeyRequest(UUID uuid, KeyRequest request)
         throws MediaDrmCallbackException {
       keyCondition.blockUninterruptible();
       keyCondition.close();
       if (alwaysFail) {
         throw new RuntimeException("executeKeyRequest failed");
       } else {
-        return new byte[0];
+        return new KeyResponse(new byte[0], FAKE_LOAD_EVENT_INFO);
       }
     }
   }

libraries/test_utils/src/main/java/androidx/media3/test/utils/FakeExoMediaDrm.java
@@ -25,6 +25,7 @@ import android.media.MediaCryptoException;
 import android.media.MediaDrmException;
 import android.media.NotProvisionedException;
 import android.media.ResourceBusyException;
+import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
@@ -34,10 +35,12 @@ import androidx.media3.common.C;
 import androidx.media3.common.DrmInitData;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
+import androidx.media3.datasource.DataSpec;
 import androidx.media3.decoder.CryptoConfig;
 import androidx.media3.exoplayer.drm.ExoMediaDrm;
 import androidx.media3.exoplayer.drm.MediaDrmCallback;
 import androidx.media3.exoplayer.drm.MediaDrmCallbackException;
+import androidx.media3.exoplayer.source.LoadEventInfo;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -46,6 +49,7 @@ import com.google.common.primitives.Bytes;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -176,6 +180,16 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
       new ProvisionRequest(TestUtil.createByteArray(7, 8, 9), "bar.test");
   public static final ImmutableList<Byte> VALID_PROVISION_RESPONSE =
       TestUtil.createByteList(4, 5, 6);
+
+  public static final LoadEventInfo FAKE_LOAD_EVENT_INFO =
+      new LoadEventInfo(
+          1,
+          new DataSpec.Builder().setUri(Uri.EMPTY).build(),
+          Uri.EMPTY,
+          Collections.emptyMap(),
+          1000,
+          2000,
+          8192);
 
   /** Key for use with the Map returned from {@link FakeExoMediaDrm#queryKeyStatus(byte[])}. */
   public static final String KEY_STATUS_KEY = "KEY_STATUS";
@@ -547,18 +561,18 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
     }
 
     @Override
-    public byte[] executeProvisionRequest(UUID uuid, ProvisionRequest request)
+    public KeyResponse executeProvisionRequest(UUID uuid, ProvisionRequest request)
         throws MediaDrmCallbackException {
       receivedProvisionRequests.add(ImmutableList.copyOf(Bytes.asList(request.getData())));
       if (Arrays.equals(request.getData(), FAKE_PROVISION_REQUEST.getData())) {
-        return Bytes.toArray(VALID_PROVISION_RESPONSE);
+        return new KeyResponse(Bytes.toArray(VALID_PROVISION_RESPONSE), FAKE_LOAD_EVENT_INFO);
       } else {
-        return Util.EMPTY_BYTE_ARRAY;
+        return new KeyResponse(Util.EMPTY_BYTE_ARRAY, FAKE_LOAD_EVENT_INFO);
       }
     }
 
     @Override
-    public byte[] executeKeyRequest(UUID uuid, KeyRequest request)
+    public KeyResponse executeKeyRequest(UUID uuid, KeyRequest request)
         throws MediaDrmCallbackException {
       ImmutableList<DrmInitData.SchemeData> schemeDatas =
           KeyRequestData.fromByteArray(request.getData()).schemeDatas;
@@ -573,7 +587,7 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
       } else {
         response = KEY_DENIED_RESPONSE;
       }
-      return Bytes.toArray(response);
+      return new KeyResponse(Bytes.toArray(response), FAKE_LOAD_EVENT_INFO);
     }
   }
 

androidx/media

libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/MediaDrmCallback.java
@@ -15,7 +15,6 @@
  */
 package androidx.media3.exoplayer.drm;
 
-import androidx.annotation.Nullable;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.exoplayer.drm.ExoMediaDrm.KeyRequest;
 import androidx.media3.exoplayer.drm.ExoMediaDrm.ProvisionRequest;
@@ -26,15 +25,23 @@ import java.util.UUID;
 @UnstableApi
 public interface MediaDrmCallback {
 
-  /** Response data from the {@link MediaDrmCallback} requests. */
+  /**
+   * Response data from the {@link MediaDrmCallback} requests.
+   *
+   * <p>Encapsulates the license server response data {@link #responseData} along with information
+   * ({@link #loadEventInfo} about the network transfer (if any) that was issued to gather the
+   * response.
+   */
   final class Response {
 
     /** The response from the license or provisioning server. */
     public final byte[] data;
+    public final LoadEventInfo loadEventInfo;
 
     /** Constructs an instance. */
-    public Response(byte[] data) {
+    public Response(byte[] data, LoadEventInfo loadEventInfo) {
       this.data = data;
+      this.loadEventInfo = loadEventInfo;
     }
   }
 
@@ -43,7 +50,7 @@ public interface MediaDrmCallback {
    *
    * @param uuid The UUID of the content protection scheme.
    * @param request The request.
-   * @return The response data.
+   * @return A {@link Response} that holds the response payload, and LoadEventInfo
    * @throws MediaDrmCallbackException If an error occurred executing the request.
    */
   Response executeProvisionRequest(UUID uuid, ProvisionRequest request)
@@ -54,23 +61,8 @@ public interface MediaDrmCallback {
    *
    * @param uuid The UUID of the content protection scheme.
    * @param request The request.
-   * @return The response data.
+   * @return A {@link Response} that holds the response payload, and LoadEventInfo
    * @throws MediaDrmCallbackException If an error occurred executing the request.
    */
   Response executeKeyRequest(UUID uuid, KeyRequest request) throws MediaDrmCallbackException;
-
-  /**
-   * Get the {@link LoadEventInfo} for the last executed request.
-   *
-   * <p>Valid after a call to {@link #executeKeyRequest(UUID, KeyRequest)} or {@link
-   * #executeProvisionRequest(UUID, ProvisionRequest)}, either contains the load event info for that
-   * request or null if no load was performed or this implementation does not support reporting load
-   * info
-   *
-   * @return the {@link LoadEventInfo} or null if no load was performed
-   */
-  @Nullable
-  default LoadEventInfo getLastLoadEventInfo() {
-    return null;
-  }
 }

libraries/exoplayer/src/test/java/androidx/media3/exoplayer/analytics/DefaultAnalyticsCollectorTest.java
@@ -75,6 +75,7 @@ import static org.robolectric.shadows.ShadowLooper.idleMainLooper;
 import static org.robolectric.shadows.ShadowLooper.runMainLooperToNextTask;
 
 import android.graphics.SurfaceTexture;
+import android.net.Uri;
 import android.os.Looper;
 import android.util.SparseArray;
 import android.view.Surface;
@@ -96,6 +97,8 @@ import androidx.media3.common.VideoSize;
 import androidx.media3.common.util.Clock;
 import androidx.media3.common.util.ConditionVariable;
 import androidx.media3.common.util.HandlerWrapper;
+import androidx.media3.common.util.Util;
+import androidx.media3.datasource.DataSpec;
 import androidx.media3.exoplayer.DecoderCounters;
 import androidx.media3.exoplayer.DecoderReuseEvaluation;
 import androidx.media3.exoplayer.ExoPlaybackException;
@@ -106,6 +109,8 @@ import androidx.media3.exoplayer.drm.DefaultDrmSessionManager;
 import androidx.media3.exoplayer.drm.DrmSession;
 import androidx.media3.exoplayer.drm.DrmSessionManager;
 import androidx.media3.exoplayer.drm.ExoMediaDrm;
+import androidx.media3.exoplayer.drm.ExoMediaDrm.KeyRequest;
+import androidx.media3.exoplayer.drm.ExoMediaDrm.ProvisionRequest;
 import androidx.media3.exoplayer.drm.MediaDrmCallback;
 import androidx.media3.exoplayer.drm.MediaDrmCallbackException;
 import androidx.media3.exoplayer.source.LoadEventInfo;
@@ -130,6 +135,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -192,6 +198,15 @@ public final class DefaultAnalyticsCollectorTest {
   private EventWindowAndPeriodId period1Seq2;
   private EventWindowAndPeriodId window0Period1Seq0;
   private EventWindowAndPeriodId window1Period0Seq1;
+  private static final LoadEventInfo FAKE_LOAD_EVENT_INFO =
+      new LoadEventInfo(
+          1,
+          new DataSpec.Builder().setUri(Uri.EMPTY).build(),
+          Uri.EMPTY,
+          Collections.emptyMap(),
+          1000,
+          2000,
+          8192);
 
   /**
    * Verify that {@link DefaultAnalyticsCollector} explicitly overrides all {@link Player.Listener}
@@ -2507,12 +2522,12 @@ public final class DefaultAnalyticsCollectorTest {
   private static final class EmptyDrmCallback implements MediaDrmCallback {
     @Override
     public Response executeProvisionRequest(UUID uuid, ExoMediaDrm.ProvisionRequest request) {
-      return new Response(new byte[0]);
+      return new Response(new byte[0], FAKE_LOAD_EVENT_INFO);
     }
 
     @Override
     public Response executeKeyRequest(UUID uuid, ExoMediaDrm.KeyRequest request) {
-      return new Response(new byte[0]);
+      return new Response(new byte[0], FAKE_LOAD_EVENT_INFO);
     }
   }
 
@@ -2553,7 +2568,7 @@ public final class DefaultAnalyticsCollectorTest {
       if (alwaysFail) {
         throw new RuntimeException("executeProvisionRequest failed");
       } else {
-        return new Response(new byte[0]);
+        return new Response(new byte[0], FAKE_LOAD_EVENT_INFO);
       }
     }
 
@@ -2565,7 +2580,7 @@ public final class DefaultAnalyticsCollectorTest {
       if (alwaysFail) {
         throw new RuntimeException("executeKeyRequest failed");
       } else {
-        return new Response(new byte[0]);
+        return new Response(new byte[0], FAKE_LOAD_EVENT_INFO);
       }
     }
   }

libraries/test_utils/src/main/java/androidx/media3/test/utils/FakeExoMediaDrm.java
@@ -26,6 +26,7 @@ import android.media.MediaCryptoException;
 import android.media.MediaDrmException;
 import android.media.NotProvisionedException;
 import android.media.ResourceBusyException;
+import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
@@ -35,10 +36,12 @@ import androidx.media3.common.C;
 import androidx.media3.common.DrmInitData;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
+import androidx.media3.datasource.DataSpec;
 import androidx.media3.decoder.CryptoConfig;
 import androidx.media3.exoplayer.drm.ExoMediaDrm;
 import androidx.media3.exoplayer.drm.MediaDrmCallback;
 import androidx.media3.exoplayer.drm.MediaDrmCallbackException;
+import androidx.media3.exoplayer.source.LoadEventInfo;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -47,6 +50,7 @@ import com.google.common.primitives.Bytes;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -177,6 +181,16 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
       new ProvisionRequest(TestUtil.createByteArray(7, 8, 9), "bar.test");
   public static final ImmutableList<Byte> VALID_PROVISION_RESPONSE =
       TestUtil.createByteList(4, 5, 6);
+
+  public static final LoadEventInfo FAKE_LOAD_EVENT_INFO =
+      new LoadEventInfo(
+          1,
+          new DataSpec.Builder().setUri(Uri.EMPTY).build(),
+          Uri.EMPTY,
+          Collections.emptyMap(),
+          1000,
+          2000,
+          8192);
 
   /** Key for use with the Map returned from {@link FakeExoMediaDrm#queryKeyStatus(byte[])}. */
   public static final String KEY_STATUS_KEY = "KEY_STATUS";
@@ -552,9 +566,9 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
         throws MediaDrmCallbackException {
       receivedProvisionRequests.add(ImmutableList.copyOf(Bytes.asList(request.getData())));
       if (Arrays.equals(request.getData(), FAKE_PROVISION_REQUEST.getData())) {
-        return new Response(Bytes.toArray(VALID_PROVISION_RESPONSE));
+        return new Response(Bytes.toArray(VALID_PROVISION_RESPONSE), FAKE_LOAD_EVENT_INFO);
       } else {
-        return new Response(Util.EMPTY_BYTE_ARRAY);
+        return new Response(Util.EMPTY_BYTE_ARRAY, FAKE_LOAD_EVENT_INFO);
       }
     }
 
@@ -574,7 +588,7 @@ public final class FakeExoMediaDrm implements ExoMediaDrm {
       } else {
         response = KEY_DENIED_RESPONSE;
       }
-      return new Response(Bytes.toArray(response));
+      return new Response(Bytes.toArray(response), FAKE_LOAD_EVENT_INFO);
     }
   }
 