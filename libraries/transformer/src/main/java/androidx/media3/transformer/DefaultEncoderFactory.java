1e164d1254b7c67618d0e2ffa0cc416f17ae522c
sheenachhabra <sheenachhabra@google.com>
2025-09-16 23:35:52 -0700

    Update bug number in TODO
    PiperOrigin-RevId: 808007255

--- libraries/transformer/src/main/java/androidx/media3/transformer/DefaultEncoderFactory.java
+++ libraries/transformer/src/main/java/androidx/media3/transformer/DefaultEncoderFactory.java
@@ -745,8 +745,7 @@ public final class DefaultEncoderFactory implements Codec.EncoderFactory {
    */
   private static void adjustMediaFormatForH264EncoderSettings(
       @Nullable ColorInfo colorInfo, MediaCodecInfo encoderInfo, MediaFormat mediaFormat) {
-    // TODO: b/210593256 - Remove overriding profile/level (before API 29) after switching to in-app
-    //  muxing.
+    // TODO: b/445454172 - Remove overriding profile/level (before API 29).
     String mimeType = MimeTypes.VIDEO_H264;
     if (SDK_INT >= 29) {
       int expectedEncodingProfile = MediaCodecInfo.CodecProfileLevel.AVCProfileHigh;
