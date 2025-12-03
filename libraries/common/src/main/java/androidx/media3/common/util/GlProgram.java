3eeb460d325ed578c33a36f19302aaac93d31af7
dancho <dancho@google.com>
2025-12-03 03:42:31 -0800

    Expand glDeleteProgram workaround for all API 28 devices
    PiperOrigin-RevId: 839685212

--- libraries/common/src/main/java/androidx/media3/common/util/GlProgram.java
+++ libraries/common/src/main/java/androidx/media3/common/util/GlProgram.java
@@ -15,7 +15,6 @@
  */
 package androidx.media3.common.util;
 
-import static android.os.Build.HARDWARE;
 import static android.os.Build.VERSION.SDK_INT;
 import static androidx.media3.common.C.TEXTURE_MIN_FILTER_LINEAR_MIPMAP_LINEAR;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -173,10 +172,14 @@ public final class GlProgram {
 
   /** Deletes the program. Deleted programs cannot be used again. */
   public void delete() throws GlUtil.GlException {
-    if (SDK_INT == 28 && HARDWARE.matches("mt67\\d{2,}")) {
+    if (SDK_INT == 28) {
       // Some MediaTek devices running API 28 crash with Fatal signal 6 (SIGABRT), code -6
       // (SI_TKILL) during glDeleteProgram. Leak the GL program instead of crashing.
       // Any leaked GL program memory will be cleaned up when the GL context is deleted.
+      // Devices with the chipset Unisoc SC9863A are also affected. Unfortunately, these devices
+      // return an extremely wide range of Build.HARDWARE values, and we cannot allow-list them
+      // all. delete() calls are frequently followed by GL context deletion, so the program memory
+      // won't leak for long.
       // See b/446675921.
       return;
     }
