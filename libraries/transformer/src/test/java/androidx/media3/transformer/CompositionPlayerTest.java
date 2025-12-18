androidx/media

commit 68ca5fceb1191fd18159d03971de66f9b638886f

Use `SpeedChangingMediaSource` for speed adjustment in `Transformer`
This change migrates speed adjustment in `Transformer` away from an
effects-based approach to a `SpeedChangingMediaSource`-based approach,
similar to `CompositionPlayer`. This migration fixes previous issues
with dropped frames in multi-item speed-adjusted sequences due to
sequence timestamps not being provided to `TimestampAdjustment`
(b/456485329).

This change also deprecates `TimestampAdjustmentShaderProgram` and
`TimestampAdjustment`, in line with efforts to restrict speed adjustment
from user-provided effects.

PiperOrigin-RevId: 844825592

libraries/transformer/src/main/java/androidx/media3/transformer/EditedMediaItem.java
@@ -16,25 +16,20 @@
 package androidx.media3.transformer;
 
 import static androidx.media3.transformer.TransformerUtil.containsSpeedChangingEffects;
-import static androidx.media3.transformer.TransformerUtil.validateSpeedChangingEffects;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static java.lang.Math.max;
 
 import androidx.annotation.IntRange;
-import androidx.annotation.Nullable;
 import androidx.media3.common.C;
 import androidx.media3.common.Effect;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.audio.AudioProcessor;
-import androidx.media3.common.audio.SpeedChangingAudioProcessor;
 import androidx.media3.common.audio.SpeedProvider;
 import androidx.media3.common.util.Log;
-import androidx.media3.common.util.SpeedProviderUtil;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
-import androidx.media3.effect.TimestampAdjustment;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.extractor.mp4.Mp4Extractor;
 import com.google.common.collect.ImmutableList;
@@ -60,7 +55,6 @@ public final class EditedMediaItem {
     private Effects effects;
     private SpeedProvider speedProvider;
     private ImmutableList<AudioProcessor> preProcessingAudioProcessors;
-    private boolean allowMatchingSpeedChangingEffectForSpeedProvider;
 
     /**
      * Creates an instance.
@@ -100,8 +94,6 @@ public final class EditedMediaItem {
       this.effects = editedMediaItem.effects;
       this.speedProvider = editedMediaItem.speedProvider;
       this.preProcessingAudioProcessors = editedMediaItem.preProcessingAudioProcessors;
-      this.allowMatchingSpeedChangingEffectForSpeedProvider =
-          editedMediaItem.allowMatchingSpeedChangingEffectForSpeedProvider;
     }
 
     /**
@@ -271,37 +263,6 @@ public final class EditedMediaItem {
     }
 
     /**
-     * Sets the provided {@link SpeedChangingAudioProcessor} and {@link TimestampAdjustment} as the
-     * first elements of their respective pipelines.
-     *
-     * <p>The effects' {@link SpeedProvider} must match the one set by {@link
-     * #setSpeed(SpeedProvider)}.
-     *
-     * @return This builder.
-     */
-    // TODO: b/456485329 - Move SpeedChangingAudioProcessor to the pre-processing pipeline.
-    @CanIgnoreReturnValue
-    /* package */ Builder setSpeedChangingEffects(
-        SpeedChangingAudioProcessor processor, @Nullable TimestampAdjustment effect) {
-      checkArgument(effect == null || processor.getSpeedProvider() == effect.speedProvider);
-      this.allowMatchingSpeedChangingEffectForSpeedProvider = true;
-      ImmutableList<AudioProcessor> audioProcessors =
-          new ImmutableList.Builder<AudioProcessor>()
-              .add(processor)
-              .addAll(effects.audioProcessors)
-              .build();
-      ImmutableList<Effect> videoEffects =
-          effect == null
-              ? effects.videoEffects
-              : new ImmutableList.Builder<Effect>()
-                  .add(effect)
-                  .addAll(effects.videoEffects)
-                  .build();
-      this.effects = new Effects(audioProcessors, videoEffects);
-      return this;
-    }
-
-    /**
      * Sets a list of {@link AudioProcessor} instances as the pre-processing pipeline for the item's
      * {@link AudioGraphInput}.
      *
@@ -364,8 +325,6 @@ public final class EditedMediaItem {
 
   /* package */ final ImmutableList<AudioProcessor> preProcessingAudioProcessors;
 
-  private final boolean allowMatchingSpeedChangingEffectForSpeedProvider;
-
   /** The duration for which this {@code EditedMediaItem} should be presented, in microseconds. */
   private long presentationDurationUs;
 
@@ -381,12 +340,7 @@ public final class EditedMediaItem {
     }
 
     if (builder.speedProvider != SpeedProvider.DEFAULT) {
-      if (builder.allowMatchingSpeedChangingEffectForSpeedProvider) {
-        checkState(validateSpeedChangingEffects(builder.effects, builder.speedProvider));
-        checkState(!containsSpeedChangingEffects(builder.effects, /* ignoreFirstEffect= */ true));
-      } else {
-        checkState(!containsSpeedChangingEffects(builder.effects, /* ignoreFirstEffect= */ false));
-      }
+      checkState(!containsSpeedChangingEffects(builder.effects, /* ignoreFirstEffect= */ false));
     }
 
     this.mediaItem = builder.mediaItem;
@@ -398,8 +352,6 @@ public final class EditedMediaItem {
     this.effects = builder.effects;
     this.speedProvider = builder.speedProvider;
     this.preProcessingAudioProcessors = builder.preProcessingAudioProcessors;
-    this.allowMatchingSpeedChangingEffectForSpeedProvider =
-        builder.allowMatchingSpeedChangingEffectForSpeedProvider;
     presentationDurationUs = C.TIME_UNSET;
   }
 
@@ -461,10 +413,6 @@ public final class EditedMediaItem {
    * @param durationUs The input duration in microseconds.
    */
   /* package */ long getDurationAfterEffectsApplied(long durationUs) {
-    if (speedProvider != SpeedProvider.DEFAULT) {
-      return SpeedProviderUtil.getDurationAfterSpeedProviderApplied(speedProvider, durationUs);
-    }
-
     long audioDurationUs = durationUs;
     long videoDurationUs = durationUs;
     if (removeAudio) {

libraries/transformer/src/test/java/androidx/media3/transformer/EditedMediaItemBuilderTest.java
@@ -232,43 +232,6 @@ public final class EditedMediaItemBuilderTest {
   }
 
   @Test
-  public void setSpeed_withSetSpeedChangingEffects_doesNotThrow() {
-    TimestampAdjustment timestampAdjustment =
-        new TimestampAdjustment((inputTimeUs, outputTimeConsumer) -> {}, SPEED_PROVIDER_2X);
-    SpeedChangingAudioProcessor processor = new SpeedChangingAudioProcessor(SPEED_PROVIDER_2X);
-    EditedMediaItem unused =
-        new EditedMediaItem.Builder(MediaItem.EMPTY)
-            .setSpeed(SPEED_PROVIDER_2X)
-            .setSpeedChangingEffects(processor, timestampAdjustment)
-            .build();
-  }
-
-  @Test
-  public void setSpeed_withSetSpeedChangingEffects_throwsWithMismatchingSpeedProviders() {
-    SpeedChangingAudioProcessor processor = new SpeedChangingAudioProcessor(SpeedProvider.DEFAULT);
-    assertThrows(
-        IllegalStateException.class,
-        () ->
-            new EditedMediaItem.Builder(MediaItem.EMPTY)
-                .setSpeed(SPEED_PROVIDER_2X)
-                .setSpeedChangingEffects(processor, /* effect= */ null)
-                .build());
-  }
-
-  @Test
-  public void setSpeedChangingEffects_withMismatchingSpeedProviders_throws() {
-    TimestampAdjustment timestampAdjustment =
-        new TimestampAdjustment((inputTimeUs, outputTimeConsumer) -> {}, SPEED_PROVIDER_2X);
-    SpeedChangingAudioProcessor processor = new SpeedChangingAudioProcessor(SpeedProvider.DEFAULT);
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            new EditedMediaItem.Builder(MediaItem.EMPTY)
-                .setSpeedChangingEffects(processor, timestampAdjustment)
-                .build());
-  }
-
-  @Test
   public void setPreProcessingAudioProcessors_populatesPreProcessingAudioProcessors() {
     ToInt16PcmAudioProcessor processor = new ToInt16PcmAudioProcessor();
     EditedMediaItem item =
@@ -281,13 +244,6 @@ public final class EditedMediaItemBuilderTest {
     EditedMediaItem duplicateItem = item.buildUpon().build();
 
     assertThat(duplicateItem.preProcessingAudioProcessors).containsExactly(processor);
-  }
-
-  @Test
-  public void getDurationAfterEffectsApplied_withSpeedProvider_returnsCorrectDuration() {
-    EditedMediaItem editedMediaItem =
-        new EditedMediaItem.Builder(MediaItem.EMPTY).setSpeed(SPEED_PROVIDER_2X).build();
-    assertThat(editedMediaItem.getDurationAfterEffectsApplied(1_000_000)).isEqualTo(500_000);
   }
 
   @Test

libraries/effect/src/main/java/androidx/media3/effect/TimestampAdjustment.java
@@ -25,8 +25,11 @@ import androidx.media3.common.util.UnstableApi;
  * Changes the frame timestamps using the {@link TimestampMap}.
  *
  * <p>This effect doesn't drop any frames.
+ *
+ * @deprecated Use {@code EditedMediaItem.Builder#setSpeed(SpeedProvider)} instead.
  */
 @UnstableApi
+@Deprecated
 public final class TimestampAdjustment implements GlEffect {
 
   /**

libraries/effect/src/main/java/androidx/media3/effect/TimestampAdjustmentShaderProgram.java
@@ -28,8 +28,13 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/** Changes the frame timestamps using the {@link TimestampMap}. */
+/**
+ * Changes the frame timestamps using the {@link TimestampMap}.
+ *
+ * @deprecated Use {@code EditedMediaItem.Builder#setSpeed(SpeedProvider)} instead.
+ */
 @UnstableApi
+@Deprecated
 public class TimestampAdjustmentShaderProgram implements GlShaderProgram {
 
   private final TimestampMap timestampMap;

commit 33ae5effeddf0de647741e8aa9dae22c891ef422

Fix getPresentationDurationUs logic for #setSpeed()
https://github.com/androidx/media/commit/68ca5fceb1191fd18159d03971de66f9b638886f broke the CompositionPlayer-related logic for calculating
the duration of EditedMediaItems that have a SpeedProvider applied on
the via EditedMediaItem.Builder#setSpeed().

This change also improves documentation and testing for duration-related
methods.

PiperOrigin-RevId: 846410081

libraries/transformer/src/main/java/androidx/media3/transformer/EditedMediaItem.java
@@ -25,9 +25,11 @@ import androidx.annotation.IntRange;
 import androidx.media3.common.C;
 import androidx.media3.common.Effect;
 import androidx.media3.common.MediaItem;
+import androidx.media3.common.MediaItem.ClippingConfiguration;
 import androidx.media3.common.audio.AudioProcessor;
 import androidx.media3.common.audio.SpeedProvider;
 import androidx.media3.common.util.Log;
+import androidx.media3.common.util.SpeedProviderUtil;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
 import androidx.media3.exoplayer.source.MediaSource;
@@ -308,8 +310,8 @@ public final class EditedMediaItem {
   public final boolean flattenForSlowMotion;
 
   /**
-   * The duration of the image in the output video for image {@link MediaItem}, or the media
-   * duration for other types of {@link MediaItem}, in microseconds.
+   * The duration of the image in the output video for image {@link MediaItem}, or the original
+   * media duration for other types of {@link MediaItem}, in microseconds.
    */
   // TODO: b/309767764 - Consider merging with presentationDurationUs.
   public final long durationUs;
@@ -365,24 +367,28 @@ public final class EditedMediaItem {
     return new Builder(this);
   }
 
+  /**
+   * Returns the presentation duration in microseconds based on the {@linkplain #durationUs original
+   * media duration}, clipping configuration, and effects applied.
+   *
+   * <p>This method is only meant to be used with {@link CompositionPlayer}, because it relies on
+   * getting the original media duration {@linkplain Builder#setDurationUs upfront}, and not from a
+   * {@link MediaSource}.
+   */
   /* package */ long getPresentationDurationUs() {
+    checkState(durationUs != C.TIME_UNSET);
     if (presentationDurationUs == C.TIME_UNSET) {
-      if (mediaItem.clippingConfiguration.equals(MediaItem.ClippingConfiguration.UNSET)
-          || durationUs == C.TIME_UNSET) {
-        // TODO: b/290734981 - Use presentationDurationUs for image presentation
-        presentationDurationUs = durationUs;
+      presentationDurationUs = getClippedDuration(mediaItem.clippingConfiguration, durationUs);
+
+      if (speedProvider != SpeedProvider.DEFAULT) {
+        presentationDurationUs =
+            SpeedProviderUtil.getDurationAfterSpeedProviderApplied(
+                speedProvider, presentationDurationUs);
       } else {
-        MediaItem.ClippingConfiguration clippingConfiguration = mediaItem.clippingConfiguration;
-        checkArgument(!clippingConfiguration.relativeToDefaultPosition);
-        if (clippingConfiguration.endPositionUs == C.TIME_END_OF_SOURCE) {
-          presentationDurationUs = durationUs - clippingConfiguration.startPositionUs;
-        } else {
-          checkArgument(clippingConfiguration.endPositionUs <= durationUs);
-          presentationDurationUs =
-              clippingConfiguration.endPositionUs - clippingConfiguration.startPositionUs;
-        }
+        // Only use when a SpeedProvider has not been set. User-provided speed-changing effects are
+        // not allowed when a SpeedProvider is set.
+        presentationDurationUs = getDurationAfterEffectsApplied(presentationDurationUs);
       }
-      presentationDurationUs = getDurationAfterEffectsApplied(presentationDurationUs);
     }
     return presentationDurationUs;
   }
@@ -396,7 +402,9 @@ public final class EditedMediaItem {
       jsonObject.put("removeAudio", removeAudio);
       jsonObject.put("removeVideo", removeVideo);
       jsonObject.put("durationUs", durationUs);
-      jsonObject.put("presentationDuration", getPresentationDurationUs());
+      // Calling getPresentationDurationUs() without a set duration will crash.
+      jsonObject.put(
+          "presentationDuration", durationUs != C.TIME_UNSET ? getPresentationDurationUs() : "N/A");
     } catch (JSONException e) {
       Log.w(/* tag= */ "EditedMediaItem", "JSON conversion failed.", e);
       return new JSONObject();
@@ -405,16 +413,20 @@ public final class EditedMediaItem {
   }
 
   /**
-   * Returns the adjusted duration in microseconds after processing {@code durationUs} input with
-   * the {@link EditedMediaItem}'s {@link Effects}.
+   * Returns the adjusted duration in microseconds after processing {@code renderedDurationUs} input
+   * with the {@link EditedMediaItem}'s {@link Effects}.
    *
    * <p>If the audio and video durations do not match, the method returns the maximum duration.
    *
-   * @param durationUs The input duration in microseconds.
+   * <p>{@code renderedDurationUs} represents the duration of the input that will be fed into the
+   * processing pipeline, <b>not</b> the duration of the original media. The {@code
+   * rendererDurationUs} incorporates modifications like clipping or {@linkplain Builder#setSpeed
+   * speed changing} that occur before samples reach {@linkplain Builder#setEffects(Effects)
+   * user-provided effects}.
    */
-  /* package */ long getDurationAfterEffectsApplied(long durationUs) {
-    long audioDurationUs = durationUs;
-    long videoDurationUs = durationUs;
+  /* package */ long getDurationAfterEffectsApplied(long renderedDurationUs) {
+    long audioDurationUs = renderedDurationUs;
+    long videoDurationUs = renderedDurationUs;
     if (removeAudio) {
       audioDurationUs = C.TIME_UNSET;
     } else {
@@ -442,6 +454,22 @@ public final class EditedMediaItem {
 
   private static boolean isGap(MediaItem mediaItem) {
     return Objects.equals(mediaItem.mediaId, GAP_MEDIA_ID);
+  }
+
+  private static long getClippedDuration(
+      ClippingConfiguration clippingConfiguration, long durationUs) {
+    if (clippingConfiguration.equals(MediaItem.ClippingConfiguration.UNSET)) {
+      // TODO: b/290734981 - Use presentationDurationUs for image presentation
+      return durationUs;
+    }
+
+    checkArgument(!clippingConfiguration.relativeToDefaultPosition);
+    if (clippingConfiguration.endPositionUs == C.TIME_END_OF_SOURCE) {
+      return durationUs - clippingConfiguration.startPositionUs;
+    } else {
+      checkArgument(clippingConfiguration.endPositionUs <= durationUs);
+      return clippingConfiguration.endPositionUs - clippingConfiguration.startPositionUs;
+    }
   }
 
   private static JSONObject jsonObjectFrom(MediaItem mediaItem) throws JSONException {

libraries/transformer/src/test/java/androidx/media3/transformer/CompositionPlayerTest.java
@@ -134,6 +134,36 @@ public class CompositionPlayerTest {
           CompositionPlayerTest::buildComposition,
           /* expectedDurationMs= */ 1_348,
           "TWO_AUDIO_ITEMS");
+  private static final Input TWO_AUDIO_ITEMS_WITH_SET_SPEED =
+      new Input(
+          () -> {
+            EditedMediaItem item1 =
+                new EditedMediaItem.Builder(MediaItem.fromUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW))
+                    .setDurationUs(1_000_000L)
+                    .setSpeed(HALF_SPEED_PROVIDER)
+                    .build();
+            EditedMediaItem item2 =
+                new EditedMediaItem.Builder(
+                        MediaItem.fromUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW_STEREO_48000KHZ))
+                    .setDurationUs(348_000L)
+                    .setSpeed(DOUBLE_SPEED_PROVIDER)
+                    .build();
+            return new Composition.Builder(withAudioFrom(ImmutableList.of(item1, item2))).build();
+          },
+          /* expectedDurationMs= */ 2_174,
+          "TWO_AUDIO_ITEMS_WITH_SET_SPEED");
+  private static final Input AUDIO_ITEM_WITH_SET_DOUBLE_SPEED =
+      new Input(
+          () -> {
+            EditedMediaItem item =
+                new EditedMediaItem.Builder(MediaItem.fromUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW))
+                    .setDurationUs(1_000_000L)
+                    .setSpeed(DOUBLE_SPEED_PROVIDER)
+                    .build();
+            return new Composition.Builder(withAudioFrom(ImmutableList.of(item))).build();
+          },
+          /* expectedDurationMs= */ 500,
+          "AUDIO_ITEM_WITH_SET_DOUBLE_SPEED");
   private static final Input AUDIO_ITEM_WITH_CLIPPED_START =
       new Input(
           () -> {
@@ -152,7 +182,6 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 800,
           "AUDIO_ITEM_WITH_CLIPPED_START");
-
   private static final Input AUDIO_ITEM_WITH_CLIPPED_START_GREATER_THAN_HALF_DURATION =
       new Input(
           () -> {
@@ -175,7 +204,6 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 400,
           "AUDIO_ITEM_WITH_CLIPPED_START_GREATER_THAN_HALF_DURATION");
-
   private static final Input AUDIO_ITEM_WITH_CLIPPED_END =
       new Input(
           () -> {
@@ -194,7 +222,6 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 600,
           "AUDIO_ITEM_WITH_CLIPPED_END");
-
   private static final Input AUDIO_ITEM_WITH_CLIPPED_START_AND_END =
       new Input(
           () -> {
@@ -214,7 +241,6 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 450,
           "AUDIO_ITEM_WITH_CLIPPED_START_AND_END");
-
   private static final Input AUDIO_ITEM_WITH_CLIPPED_START_AND_DOUBLE_SPEED_EFFECT =
       new Input(
           () -> {
@@ -241,7 +267,27 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 400,
           "AUDIO_ITEM_WITH_CLIPPED_START_AND_DOUBLE_SPEED_EFFECT");
-
+  private static final Input AUDIO_ITEM_WITH_CLIPPED_START_AND_SET_DOUBLE_SPEED =
+      new Input(
+          () -> {
+            MediaItem mediaItem =
+                new MediaItem.Builder()
+                    .setUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW)
+                    .setClippingConfiguration(
+                        new MediaItem.ClippingConfiguration.Builder()
+                            .setStartPositionUs(200_000)
+                            .build())
+                    .build();
+            EditedMediaItem editedMediaItem =
+                new EditedMediaItem.Builder(mediaItem)
+                    .setDurationUs(1_000_000L)
+                    .setSpeed(DOUBLE_SPEED_PROVIDER)
+                    .build();
+            return new Composition.Builder(withAudioFrom(ImmutableList.of(editedMediaItem)))
+                .build();
+          },
+          /* expectedDurationMs= */ 400,
+          "AUDIO_ITEM_WITH_CLIPPED_START_AND_SET_DOUBLE_SPEED");
   private static final Input AUDIO_ITEM_WITH_CLIPPED_END_AND_DOUBLE_SPEED_EFFECT =
       new Input(
           () -> {
@@ -268,7 +314,27 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 300,
           "AUDIO_ITEM_WITH_CLIPPED_END_AND_DOUBLE_SPEED_EFFECT");
-
+  private static final Input AUDIO_ITEM_WITH_CLIPPED_END_AND_SET_DOUBLE_SPEED =
+      new Input(
+          () -> {
+            MediaItem mediaItem =
+                new MediaItem.Builder()
+                    .setUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW)
+                    .setClippingConfiguration(
+                        new MediaItem.ClippingConfiguration.Builder()
+                            .setEndPositionUs(600_000)
+                            .build())
+                    .build();
+            EditedMediaItem editedMediaItem =
+                new EditedMediaItem.Builder(mediaItem)
+                    .setDurationUs(1_000_000L)
+                    .setSpeed(DOUBLE_SPEED_PROVIDER)
+                    .build();
+            return new Composition.Builder(withAudioFrom(ImmutableList.of(editedMediaItem)))
+                .build();
+          },
+          /* expectedDurationMs= */ 300,
+          "AUDIO_ITEM_WITH_CLIPPED_END_AND_SET_DOUBLE_SPEED");
   private static final Input AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_HALF_SPEED_EFFECT =
       new Input(
           () -> {
@@ -291,6 +357,28 @@ public class CompositionPlayerTest {
           },
           /* expectedDurationMs= */ 900,
           "AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_HALF_SPEED_EFFECT");
+  private static final Input AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_SET_HALF_SPEED =
+      new Input(
+          () -> {
+            MediaItem mediaItem =
+                new MediaItem.Builder()
+                    .setUri(ASSET_URI_PREFIX + FILE_AUDIO_RAW)
+                    .setClippingConfiguration(
+                        new MediaItem.ClippingConfiguration.Builder()
+                            .setStartPositionUs(100_000)
+                            .setEndPositionUs(550_000)
+                            .build())
+                    .build();
+            EditedMediaItem editedMediaItem =
+                new EditedMediaItem.Builder(mediaItem)
+                    .setDurationUs(1_000_000L)
+                    .setSpeed(HALF_SPEED_PROVIDER)
+                    .build();
+            EditedMediaItemSequence sequence = withAudioFrom(ImmutableList.of(editedMediaItem));
+            return new Composition.Builder(sequence).build();
+          },
+          /* expectedDurationMs= */ 900,
+          "AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_SET_HALF_SPEED");
 
   @Test
   public void builder_buildCalledTwice_throws() {
@@ -1161,13 +1249,18 @@ public class CompositionPlayerTest {
     protected List<Input> provideValues(TestParameterValuesProvider.Context context) {
       return ImmutableList.of(
           TWO_AUDIO_ITEMS,
+          TWO_AUDIO_ITEMS_WITH_SET_SPEED,
+          AUDIO_ITEM_WITH_SET_DOUBLE_SPEED,
           AUDIO_ITEM_WITH_CLIPPED_START,
           AUDIO_ITEM_WITH_CLIPPED_START_GREATER_THAN_HALF_DURATION,
           AUDIO_ITEM_WITH_CLIPPED_END,
           AUDIO_ITEM_WITH_CLIPPED_START_AND_END,
           AUDIO_ITEM_WITH_CLIPPED_START_AND_DOUBLE_SPEED_EFFECT,
+          AUDIO_ITEM_WITH_CLIPPED_START_AND_SET_DOUBLE_SPEED,
           AUDIO_ITEM_WITH_CLIPPED_END_AND_DOUBLE_SPEED_EFFECT,
-          AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_HALF_SPEED_EFFECT);
+          AUDIO_ITEM_WITH_CLIPPED_END_AND_SET_DOUBLE_SPEED,
+          AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_HALF_SPEED_EFFECT,
+          AUDIO_ITEM_WITH_CLIPPED_START_AND_END_WITH_SET_HALF_SPEED);
     }
   }
 

libraries/transformer/src/test/java/androidx/media3/transformer/EditedMediaItemBuilderTest.java
@@ -51,6 +51,25 @@ public final class EditedMediaItemBuilderTest {
         }
       };
 
+  private static final SpeedProvider SPEED_PROVIDER_WITH_SPEED_UP_AND_SLOW_DOWN =
+      new SpeedProvider() {
+        @Override
+        public float getSpeed(long timeUs) {
+          if (timeUs >= 5_000_000) {
+            return 2f;
+          }
+          return 0.25f;
+        }
+
+        @Override
+        public long getNextSpeedChangeTimeUs(long timeUs) {
+          if (timeUs >= 5_000_000) {
+            return C.TIME_UNSET;
+          }
+          return 5_000_000;
+        }
+      };
+
   @Test
   public void build_removeAudioAndVideo_throws() {
     MediaItem mediaItem = MediaItem.fromUri("uri");
@@ -205,6 +224,40 @@ public final class EditedMediaItemBuilderTest {
   }
 
   @Test
+  public void getPresentationDurationUs_withSetSpeed_returnsSpeedAdjustedDuration() {
+    EditedMediaItem item =
+        new EditedMediaItem.Builder(MediaItem.EMPTY)
+            .setDurationUs(10_000_000)
+            .setSpeed(SPEED_PROVIDER_WITH_SPEED_UP_AND_SLOW_DOWN)
+            .build();
+
+    assertThat(item.getPresentationDurationUs()).isEqualTo(22_500_000);
+  }
+
+  @Test
+  public void getPresentationDurationUs_withSetSpeedAndClipping_returnsSpeedAdjustedDuration() {
+    MediaItem.ClippingConfiguration clippingConfiguration =
+        new MediaItem.ClippingConfiguration.Builder()
+            .setStartPositionUs(1_000_000)
+            .setEndPositionUs(8_000_000)
+            .build();
+    EditedMediaItem item =
+        new EditedMediaItem.Builder(
+                MediaItem.EMPTY.buildUpon().setClippingConfiguration(clippingConfiguration).build())
+            .setDurationUs(10_000_000)
+            .setSpeed(SPEED_PROVIDER_WITH_SPEED_UP_AND_SLOW_DOWN)
+            .build();
+
+    assertThat(item.getPresentationDurationUs()).isEqualTo(21_000_000);
+  }
+
+  @Test
+  public void getPresentationDurationUs_withDurationUnset_throws() {
+    EditedMediaItem item = new EditedMediaItem.Builder(MediaItem.EMPTY).build();
+    assertThrows(IllegalStateException.class, item::getPresentationDurationUs);
+  }
+
+  @Test
   public void setSpeed_withSpeedChangingAudioProcessor_throws() {
     SpeedChangingAudioProcessor processor = new SpeedChangingAudioProcessor(SpeedProvider.DEFAULT);
     Effects effects = new Effects(ImmutableList.of(processor), ImmutableList.of());
@@ -270,6 +323,13 @@ public final class EditedMediaItemBuilderTest {
         new EditedMediaItem.Builder(MediaItem.EMPTY)
             .setEffects(new Effects(ImmutableList.of(), ImmutableList.of(effects.second)))
             .build();
+    assertThat(editedMediaItem.getDurationAfterEffectsApplied(1_000_000)).isEqualTo(1_000_000);
+  }
+
+  @Test
+  public void getDurationAfterEffectsApplied_withSetSpeed_returnsInputDuration() {
+    EditedMediaItem editedMediaItem =
+        new EditedMediaItem.Builder(MediaItem.EMPTY).setSpeed(SPEED_PROVIDER_2X).build();
     assertThat(editedMediaItem.getDurationAfterEffectsApplied(1_000_000)).isEqualTo(1_000_000);
   }
 }