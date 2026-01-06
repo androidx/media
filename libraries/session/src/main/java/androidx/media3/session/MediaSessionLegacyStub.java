f2eb4694a426db312651c2ff8604ecfc2a5a235a
tonihei <tonihei@google.com>
2026-01-06 03:49:42 -0800

    Set full AudioAttributes for local media sessions
    We currently convert to the less expressive volume control
    stream, only to convert back to AudioAttributes.
    
    PiperOrigin-RevId: 852711032

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
@@ -1659,7 +1659,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       @DeviceInfo.PlaybackType
       int playbackType = sessionImpl.getPlayerWrapper().getDeviceInfo().playbackType;
       if (playbackType == DeviceInfo.PLAYBACK_TYPE_LOCAL) {
-        sessionCompat.setPlaybackToLocal(audioAttributes.getVolumeControlStream());
+        sessionCompat.setPlaybackToLocal(audioAttributes);
       }
     }
 
@@ -1668,8 +1668,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       PlayerWrapper player = sessionImpl.getPlayerWrapper();
       volumeProviderCompat = createVolumeProviderCompat(player);
       if (volumeProviderCompat == null) {
-        int streamType = player.getAudioAttributesWithCommandCheck().getVolumeControlStream();
-        sessionCompat.setPlaybackToLocal(streamType);
+        sessionCompat.setPlaybackToLocal(player.getAudioAttributesWithCommandCheck());
       } else {
         sessionCompat.setPlaybackToRemote(volumeProviderCompat);
       }
