darulkirom/M3ExoPlayer

Update notification when device info changes
Device info determines whether we have cast or normal media
notification. It decides whether we can enable playback resumption.
To ensure the state is correct, notify SystemUI when device info
changed.

libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
@@ -1673,7 +1673,10 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       int playbackType = sessionImpl.getPlayerWrapper().getDeviceInfo().playbackType;
       if (playbackType == DeviceInfo.PLAYBACK_TYPE_LOCAL) {
         postOrRunForCompatSession(
-            () -> sessionCompat.setPlaybackToLocal(audioAttributes.getStreamType()));
+            () -> {
+              sessionCompat.setPlaybackToLocal(audioAttributes.getStreamType());
+              sessionImpl.onNotificationRefreshRequired();
+            });
       }
     }
 
@@ -1683,9 +1686,15 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       volumeProviderCompat = createVolumeProviderCompat(player);
       if (volumeProviderCompat == null) {
         int streamType = player.getAudioAttributesWithCommandCheck().getStreamType();
-        postOrRunForCompatSession(() -> sessionCompat.setPlaybackToLocal(streamType));
+        postOrRunForCompatSession(() -> {
+          sessionCompat.setPlaybackToLocal(streamType);
+          sessionImpl.onNotificationRefreshRequired();
+        });
       } else {
-        postOrRunForCompatSession(() -> sessionCompat.setPlaybackToRemote(volumeProviderCompat));
+        postOrRunForCompatSession(() -> {
+          sessionCompat.setPlaybackToRemote(volumeProviderCompat);
+          sessionImpl.onNotificationRefreshRequired();
+        });
       }
     }
 

androidx/media

libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
@@ -1742,7 +1742,10 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       int playbackType = sessionImpl.getPlayerWrapper().getDeviceInfo().playbackType;
       if (playbackType == DeviceInfo.PLAYBACK_TYPE_LOCAL) {
         postOrRunForCompatSession(
-            () -> sessionCompat.setPlaybackToLocal(audioAttributes));
+            () -> {
+              sessionCompat.setPlaybackToLocal(audioAttributes);
+              sessionImpl.onNotificationRefreshRequired();
+            });
       }
     }
 
@@ -1752,9 +1755,15 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       volumeProviderCompat = createVolumeProviderCompat(player);
       if (volumeProviderCompat == null) {
         AudioAttributes audioAttributes = player.getAudioAttributesWithCommandCheck();
-        postOrRunForCompatSession(() -> sessionCompat.setPlaybackToLocal(audioAttributes));
+        postOrRunForCompatSession(() -> {
+          sessionCompat.setPlaybackToLocal(audioAttributes);
+          sessionImpl.onNotificationRefreshRequired();
+        });
       } else {
-        postOrRunForCompatSession(() -> sessionCompat.setPlaybackToRemote(volumeProviderCompat));
+        postOrRunForCompatSession(() -> {
+          sessionCompat.setPlaybackToRemote(volumeProviderCompat);
+          sessionImpl.onNotificationRefreshRequired();
+        });
       }
     }
 