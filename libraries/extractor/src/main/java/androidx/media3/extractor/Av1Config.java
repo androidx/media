674f0abb7325f65daeae45d4cdc64b5be26baeee
ibaker <ibaker@google.com>
2026-03-13 05:58:53 -0700

    Simplify control flow in `Av1Config`
    This change makes most locals 'effectively final' and moves their
    declarations closer to their usage sites.
    
    It also removes the `seq_profile` condition when calculating the bit
    depth from the `AV1CodecConfigurationRecord`. This conditional logic
    is needed when **parsing** the bit depth from a `color_config` OBU
    (see section 5.5.2 of the [AV1 spec](https://aomediacodec.github.io/av1-spec/av1-spec.pdf#page=44).
    The conditional logic is not needed when handling the values read
    from the `AV1CodecConfigurationRecord` because its
    [semantics](https://aomediacodec.github.io/av1-isobmff/#av1codecconfigurationbox-semantics)
    are defined as (emphasis mine):
    > The `high_bitdepth` field SHALL be equal to the value of
    > `high_bitdepth` flag from the Sequence Header OBU. The `twelve_bit`
    > field SHALL be equal to the value of the `twelve_bit` flag from the
    > Sequence Header OBU. **When `twelve_bit` is not present in the
    > Sequence Header OBU the `AV1CodecConfigurationRecord` `twelve_bit`
    > value SHALL be 0.**
    
    PiperOrigin-RevId: 883121220

--- libraries/extractor/src/main/java/androidx/media3/extractor/Av1Config.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/Av1Config.java
@@ -16,6 +16,9 @@
 package androidx.media3.extractor;
 
 import androidx.media3.common.C;
+import androidx.media3.common.C.ColorRange;
+import androidx.media3.common.C.ColorSpace;
+import androidx.media3.common.C.ColorTransfer;
 import androidx.media3.common.ColorInfo;
 import androidx.media3.common.Format;
 import androidx.media3.common.ParserException;
@@ -74,25 +77,21 @@ public final class Av1Config {
    */
   public static Av1Config parse(byte[] initializationData) throws ParserException {
     try {
-      int bitdepth = Format.NO_VALUE;
-      int colorSpace = Format.NO_VALUE;
-      int colorRange = Format.NO_VALUE;
-      int colorTransfer = Format.NO_VALUE;
-
       // We can wrap the raw bytes directly in a ParsableBitArray, starting at position 0!
       ParsableBitArray bitArray = new ParsableBitArray(initializationData);
 
       // Parse av1C config record for bitdepth info.
       // See https://aomediacodec.github.io/av1-isobmff/#av1codecconfigurationbox-syntax.
       bitArray.skipBytes(1); // marker, version
-      int seqProfile = bitArray.readBits(3); // seq_profile
+      bitArray.skipBits(3); // seq_profile
       bitArray.skipBits(6); // seq_level_idx_0, seq_tier_0
       boolean highBitdepth = bitArray.readBit(); // high_bitdepth
       boolean twelveBit = bitArray.readBit(); // twelve_bit
-      if (seqProfile == 2 && highBitdepth) {
+      int bitdepth;
+      if (highBitdepth) {
         bitdepth = twelveBit ? 12 : 10;
-      } else if (seqProfile <= 2) {
-        bitdepth = highBitdepth ? 10 : 8;
+      } else {
+        bitdepth = 8;
       }
       // Skip monochrome, chroma_subsampling_x, chroma_subsampling_y, chroma_sample_position,
       // reserved and initial_presentation_delay.
@@ -100,8 +99,7 @@ public final class Av1Config {
 
       // The configOBUs array is optional. If the payload is exactly 4 bytes, we are done.
       if (bitArray.bitsLeft() <= 0) {
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
 
       // 5.3.1. General OBU syntax
@@ -109,13 +107,11 @@ public final class Av1Config {
       int obuType = bitArray.readBits(4); // obu_type
       if (obuType != 1) { // obu_type != OBU_SEQUENCE_HEADER
         Log.i(TAG, "Unsupported obu_type: " + obuType);
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       if (bitArray.readBit()) { // obu_extension_flag
         Log.i(TAG, "Unsupported obu_extension_flag");
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       boolean obuHasSizeField = bitArray.readBit(); // obu_has_size_field
       bitArray.skipBit(); // obu_reserved_1bit
@@ -123,26 +119,22 @@ public final class Av1Config {
       // readBits(8).
       if (obuHasSizeField && bitArray.readBits(8) > 127) { // obu_size
         Log.i(TAG, "Excessive obu_size");
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       // 5.5.1. General OBU sequence header syntax
       int obuSeqHeaderSeqProfile = bitArray.readBits(3); // seq_profile
       bitArray.skipBit(); // still_picture
       if (bitArray.readBit()) { // reduced_still_picture_header
         Log.i(TAG, "Unsupported reduced_still_picture_header");
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       if (bitArray.readBit()) { // timing_info_present_flag
         Log.i(TAG, "Unsupported timing_info_present_flag");
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       if (bitArray.readBit()) { // initial_display_delay_present_flag
         Log.i(TAG, "Unsupported initial_display_delay_present_flag");
-        return new Av1Config(
-            ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+        return new Av1Config(initializationData, bitdepth);
       }
       int operatingPointsCountMinus1 = bitArray.readBits(5); // operating_points_cnt_minus_1
       for (int i = 0; i <= operatingPointsCountMinus1; i++) {
@@ -185,38 +177,48 @@ public final class Av1Config {
 
       boolean monochrome = (obuSeqHeaderSeqProfile != 1) && bitArray.readBit(); // mono_chrome
 
-      if (bitArray.readBit()) { // color_description_present_flag
-        int colorPrimaries = bitArray.readBits(8); // color_primaries
-        int transferCharacteristics = bitArray.readBits(8); // transfer_characteristics
-        int matrixCoefficients = bitArray.readBits(8); // matrix_coefficients
-        int colorRangeValue =
-            (!monochrome
-                    && colorPrimaries == 1 // CP_BT_709
-                    && transferCharacteristics == 13 // TC_SRGB
-                    && matrixCoefficients == 0) // MC_IDENTITY
-                ? 1
-                : bitArray.readBits(1); // color_range;
-        colorSpace = ColorInfo.isoColorPrimariesToColorSpace(colorPrimaries);
-        colorRange = (colorRangeValue == 1) ? C.COLOR_RANGE_FULL : C.COLOR_RANGE_LIMITED;
-        colorTransfer =
-            ColorInfo.isoTransferCharacteristicsToColorTransfer(transferCharacteristics);
+      if (!bitArray.readBit()) { // color_description_present_flag
+        return new Av1Config(initializationData, bitdepth);
       }
-
-      return new Av1Config(
-          ImmutableList.of(initializationData), bitdepth, colorSpace, colorRange, colorTransfer);
+      int colorPrimaries = bitArray.readBits(8); // color_primaries
+      int transferCharacteristics = bitArray.readBits(8); // transfer_characteristics
+      int matrixCoefficients = bitArray.readBits(8); // matrix_coefficients
+      int colorRangeValue =
+          (!monochrome
+                  && colorPrimaries == 1 // CP_BT_709
+                  && transferCharacteristics == 13 // TC_SRGB
+                  && matrixCoefficients == 0) // MC_IDENTITY
+              ? 1
+              : bitArray.readBits(1); // color_range;
+      @ColorSpace int colorSpace = ColorInfo.isoColorPrimariesToColorSpace(colorPrimaries);
+      @ColorRange
+      int colorRange = (colorRangeValue == 1) ? C.COLOR_RANGE_FULL : C.COLOR_RANGE_LIMITED;
+      @ColorTransfer
+      int colorTransfer =
+          ColorInfo.isoTransferCharacteristicsToColorTransfer(transferCharacteristics);
+      return new Av1Config(initializationData, bitdepth, colorSpace, colorRange, colorTransfer);
 
     } catch (RuntimeException e) {
       throw ParserException.createForMalformedContainer("Error parsing AV1 config", e);
     }
   }
 
+  private Av1Config(byte[] initializationData, int bitdepth) {
+    this(
+        initializationData,
+        bitdepth,
+        /* colorSpace= */ Format.NO_VALUE,
+        /* colorRange= */ Format.NO_VALUE,
+        /* colorTransfer= */ Format.NO_VALUE);
+  }
+
   private Av1Config(
-      List<byte[]> initializationData,
+      byte[] initializationData,
       int bitdepth,
       @C.ColorSpace int colorSpace,
       @C.ColorRange int colorRange,
       @C.ColorTransfer int colorTransfer) {
-    this.initializationData = initializationData;
+    this.initializationData = ImmutableList.of(initializationData);
     this.bitdepth = bitdepth;
     this.colorSpace = colorSpace;
     this.colorRange = colorRange;
