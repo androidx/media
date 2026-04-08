androidx/media

commit f21c96763e68221ecc768ddaaee12de215706c4e

Move build.gradle groovy to Kotlin DSL
Move constants.gradle to libs.versions.toml catalog

* https://developer.android.com/build/migrate-to-kotlin-dsl
* https://developer.android.com/build/migrate-to-catalogs

Most `build.gradle` files are now `build.gradle.kts` apart from `common_config.gradle`. This is to avoid static typechecking for the script that will be imported both from libraries and applications. Instead, we will rely on groovy's dynamic typing and `apply from` calls from the relevant callsites.

PiperOrigin-RevId: 895925697

libraries/decoder_midi/build.gradle.kts
@@ -1,0 +1,46 @@
+// Copyright 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+plugins { alias(libs.plugins.android.library) }
+
+apply(from = "${gradle.extra["androidxMediaSettingsDir"]}/common_config.gradle")
+
+android {
+  namespace = "androidx.media3.decoder.midi"
+
+  sourceSets { getByName("androidTest").assets.srcDir("../test_data/src/test/assets") }
+
+  publishing { singleVariant("release") { withSourcesJar() } }
+}
+
+dependencies {
+  api(project(modulePrefix + "lib-exoplayer"))
+  api(project(modulePrefix + "lib-decoder"))
+  api(project(modulePrefix + "lib-extractor"))
+  api(project(modulePrefix + "lib-common"))
+  implementation(libs.androidx.annotation)
+  // Jsyn v17.2.0
+  implementation("com.github.philburk:jsyn:3f6b44b853bccc0d2e3027104d575fcc5ccb6d4e")
+  compileOnly(libs.checkerframework.qual)
+  testImplementation(libs.androidx.test.core)
+  testImplementation(libs.androidx.test.ext.junit)
+  testImplementation(project(modulePrefix + "test-utils"))
+  testImplementation(project(modulePrefix + "test-data"))
+  testImplementation(libs.robolectric)
+}
+
+extra["releaseArtifactId"] = "media3-exoplayer-midi"
+
+extra["releaseName"] = "Media3 MIDI decoder module"
+
+apply(from = "../../publish.gradle")

build.gradle.kts
@@ -1,0 +1,74 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import java.io.File
+
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    // Use the legacy 'buildscript' classpath mechanism instead of the modern 'plugins' block
+    // because version 1.2.4 of the strict-version-matcher-plugin is currently only available
+    // as a Maven artifact (com.google.android.gms:strict-version-matcher-plugin) and is not
+    // yet published to the Gradle Plugin Portal in a compatible form (only 1.2.2 so far).
+    // If version 1.2.4 (or newer) becomes available on the Plugin Portal in the future,
+    // this can be safely migrated to a standard 'id(...) alias(...)' declaration.
+    // Naming mismatch reference:
+    // https://maven.google.com/web/index.html#com.google.android.gms:strict-version-matcher-plugin
+    // https://maven.google.com/web/index.html#com.google.android.gms.strict-version-matcher-plugin
+    classpath(libs.strict.version.matcher.plugin)
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    mavenCentral()
+    maven {
+      url = uri("https://jitpack.io")
+      content { includeGroup("com.github.philburk") }
+    }
+  }
+  if (project.hasProperty("externalBuildDir")) {
+    val externalBuildDirProp = project.property("externalBuildDir") as String
+    val externalBuildDirFile =
+      if (File(externalBuildDirProp).isAbsolute) {
+        File(externalBuildDirProp)
+      } else {
+        File(rootDir, externalBuildDirProp)
+      }
+    layout.buildDirectory.set(File(externalBuildDirFile, project.name))
+  }
+  group = "androidx.media3"
+}
+
+tasks.register("printReleaseArtifactIds") {
+  description = "Prints the releaseArtifactId of modules configured for publishing."
+  doLast {
+    subprojects {
+      // Check if the project is intended to be published by looking for a task
+      // added by the media/publish.gradle script.
+      if (!tasks.names.contains("generatePomFileForReleasePublication")) {
+        return@subprojects
+      }
+
+      if (extra.has("releaseArtifactId")) {
+        println(extra["releaseArtifactId"])
+      } else {
+        logger.warn("WARN: Project ${path} has publish task but no releaseArtifactId.")
+      }
+    }
+  }
+}

buildSrc/build.gradle.kts
@@ -1,0 +1,22 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}

demos/cast/build.gradle.kts
@@ -1,0 +1,63 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+apply(from = "../../constants.gradle")
+
+plugins {
+  alias(libs.plugins.android.application)
+  id("com.google.android.gms.strict-version-matcher-plugin")
+}
+
+android {
+  namespace = "androidx.media3.demo.cast"
+
+  compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  defaultConfig {
+    versionName = releaseVersion
+    versionCode = releaseVersionCode
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.appTargetSdkVersion.get().toInt()
+  }
+
+  buildTypes {
+    getByName("release") {
+      isShrinkResources = true
+      isMinifyEnabled = true
+      proguardFiles("proguard-rules.txt", getDefaultProguardFile("proguard-android-optimize.txt"))
+      signingConfig = signingConfigs.getByName("debug")
+    }
+    getByName("debug") { isJniDebuggable = true }
+  }
+
+  // The demo app isn't indexed and doesn't have translations.
+  lint.disable += listOf("GoogleAppIndexingWarning", "MissingTranslation")
+}
+
+dependencies {
+  implementation(project(modulePrefix + "lib-exoplayer"))
+  implementation(project(modulePrefix + "lib-exoplayer-dash"))
+  implementation(project(modulePrefix + "lib-exoplayer-hls"))
+  implementation(project(modulePrefix + "lib-exoplayer-rtsp"))
+  implementation(project(modulePrefix + "lib-exoplayer-smoothstreaming"))
+  implementation(project(modulePrefix + "lib-ui"))
+  implementation(project(modulePrefix + "lib-cast"))
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.recyclerview)
+  implementation(libs.material)
+}

demos/compose/build.gradle.kts
@@ -1,0 +1,71 @@
+// Copyright 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+apply(from = "../../constants.gradle")
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose.compiler)
+}
+
+android {
+  namespace = "androidx.media3.demo.compose"
+  compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions { jvmTarget = "1.8" }
+
+  defaultConfig {
+    versionName = releaseVersion
+    versionCode = releaseVersionCode
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.appTargetSdkVersion.get().toInt()
+  }
+
+  buildTypes {
+    getByName("release") {
+      isShrinkResources = true
+      isMinifyEnabled = true
+      signingConfig = signingConfigs.getByName("debug")
+    }
+    getByName("debug") { isJniDebuggable = true }
+  }
+
+  // The demo app isn't indexed, and doesn't have translations.
+  lint.disable += listOf("GoogleAppIndexingWarning", "MissingTranslation")
+
+  buildFeatures { compose = true }
+
+  testOptions { unitTests { isIncludeAndroidResources = true } }
+}
+
+dependencies {
+  implementation("androidx.activity:activity-compose:1.9.0")
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.lifecycle.runtime.compose)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.material)
+  implementation(libs.kotlinx.coroutines.guava)
+
+  implementation(project(modulePrefix + "lib-exoplayer"))
+  implementation(project(modulePrefix + "lib-inspector"))
+  implementation(project(modulePrefix + "lib-ui-compose-material3"))
+
+  // For detecting and debugging leaks only. LeakCanary is not needed for demo app to work.
+  debugImplementation(libs.leakcanary.android)
+}

demos/composition/build.gradle.kts
@@ -1,0 +1,85 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+apply(from = "../../constants.gradle")
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose.compiler)
+}
+
+android {
+  namespace = "androidx.media3.demo.composition"
+
+  compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions { jvmTarget = "1.8" }
+
+  defaultConfig {
+    versionName = releaseVersion
+    versionCode = releaseVersionCode
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.appTargetSdkVersion.get().toInt()
+  }
+
+  buildTypes {
+    getByName("release") {
+      isShrinkResources = true
+      isMinifyEnabled = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt")
+      signingConfig = signingConfigs.getByName("debug")
+    }
+  }
+
+  // The demo app isn't indexed, and doesn't have translations.
+  lint.disable += listOf("GoogleAppIndexingWarning", "MissingTranslation")
+
+  buildFeatures { compose = true }
+}
+
+dependencies {
+  implementation(libs.androidx.appcompat)
+  implementation(libs.material)
+  implementation(project(modulePrefix + "lib-effect"))
+  implementation(project(modulePrefix + "lib-effect-ndk"))
+  implementation(project(modulePrefix + "lib-exoplayer"))
+  implementation(project(modulePrefix + "lib-exoplayer-dash"))
+  implementation(project(modulePrefix + "lib-muxer"))
+  implementation(project(modulePrefix + "lib-transformer"))
+  implementation(project(modulePrefix + "lib-inspector"))
+  implementation(project(modulePrefix + "lib-ui"))
+  implementation(project(modulePrefix + "lib-ui-compose"))
+  implementation(libs.androidx.annotation)
+  implementation(libs.androidx.lifecycle.viewmodel)
+  implementation("androidx.activity:activity-compose:1.9.0")
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.material3.adaptive)
+  implementation(libs.androidx.compose.material3.adaptive.layout)
+  implementation(libs.androidx.compose.material3.adaptive.navigation)
+  implementation(libs.kotlinx.coroutines.guava)
+  compileOnly(libs.checkerframework.qual)
+  debugImplementation(libs.androidx.compose.ui.tooling)
+  debugImplementation(libs.androidx.compose.ui.test.manifest)
+}

demos/effect/build.gradle.kts
@@ -1,0 +1,72 @@
+// Copyright 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+apply(from = "../../constants.gradle")
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose.compiler)
+}
+
+android {
+  namespace = "androidx.media3.demo.effect"
+
+  compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions { jvmTarget = "1.8" }
+
+  defaultConfig {
+    versionName = releaseVersion
+    versionCode = releaseVersionCode
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.appTargetSdkVersion.get().toInt()
+  }
+
+  buildTypes {
+    getByName("release") {
+      isShrinkResources = true
+      isMinifyEnabled = true
+      signingConfig = signingConfigs.getByName("debug")
+    }
+    getByName("debug") { isJniDebuggable = true }
+  }
+
+  // The demo app isn't indexed, and doesn't have translations.
+  lint.disable += listOf("GoogleAppIndexingWarning", "MissingTranslation")
+
+  buildFeatures { compose = true }
+  testOptions { unitTests { isIncludeAndroidResources = true } }
+}
+
+dependencies {
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.foundation)
+  implementation(libs.androidx.compose.material3)
+
+  implementation("androidx.activity:activity-compose:1.9.0")
+  implementation(libs.material)
+
+  implementation(project(modulePrefix + "lib-exoplayer"))
+  implementation(project(modulePrefix + "lib-ui"))
+  implementation(project(modulePrefix + "lib-effect"))
+  implementation(project(modulePrefix + "lib-effect-lottie"))
+
+  // For detecting and debugging leaks only. LeakCanary is not needed for demo app to work.
+  debugImplementation(libs.leakcanary.android)
+}

demos/gl/build.gradle.kts
@@ -1,0 +1,57 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+apply(from = "../../constants.gradle")
+
+plugins { alias(libs.plugins.android.application) }
+
+android {
+  namespace = "androidx.media3.demo.gl"
+
+  compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  defaultConfig {
+    versionName = releaseVersion
+    versionCode = releaseVersionCode
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.appTargetSdkVersion.get().toInt()
+  }
+
+  buildTypes {
+    getByName("release") {
+      isShrinkResources = true
+      isMinifyEnabled = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+      signingConfig = signingConfigs.getByName("debug")
+    }
+  }
+
+  // The demo app doesn't have translations.
+  lint.disable += "MissingTranslation"
+}
+
+dependencies {
+  implementation(project(modulePrefix + "lib-exoplayer"))
+  implementation(project(modulePrefix + "lib-exoplayer-dash"))
+  implementation(project(modulePrefix + "lib-exoplayer-hls"))
+  implementation(project(modulePrefix + "lib-exoplayer-rtsp"))
+  implementation(project(modulePrefix + "lib-exoplayer-smoothstreaming"))
+  implementation(project(modulePrefix + "lib-ui"))
+  implementation(libs.androidx.annotation)
+  compileOnly(libs.checkerframework.qual)
+}

commit 3decb95f418fc415c382c1fde6cfd0661a6bdeef

Fix `sourceSets` config of `lib-decoder-midi`
This was broken by https://github.com/androidx/media/commit/f21c96763e68221ecc768ddaaee12de215706c4e

PiperOrigin-RevId: 896490604

libraries/decoder_midi/build.gradle.kts
@@ -18,7 +18,7 @@ apply(from = "${gradle.extra["androidxMediaSettingsDir"]}/common_config.gradle")
 android {
   namespace = "androidx.media3.decoder.midi"
 
-  sourceSets { getByName("androidTest").assets.srcDir("../test_data/src/test/assets") }
+  sourceSets { getByName("test").assets.srcDir("../test_data/src/test/assets") }
 
   publishing { singleVariant("release") { withSourcesJar() } }
 }