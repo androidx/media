e5d1d3ce427d6b70c7959e77eadd85596d16a6ad
jbibik <jbibik@google.com>
2026-04-08 08:03:44 -0700

    Declare plugins in project-level build.gradle.kts
    According to https://docs.gradle.org/current/userguide/plugins_intermediate.html#sec:subprojects_plugins_dsl, in multi-module builds, we want to declare the plugins in the root block, but `apply false`. The subprojects can then actually apply them and pull the version out of the libs.toml catalog.
    
    Without this change, the build prints a warning
    
    ```
    The Kotlin Gradle plugin was loaded multiple times in different subprojects,
    which is not supported and may break the build.
    This might happen in subprojects that apply the Kotlin plugins with the Gradle plugins { ... }
    if they specify explicit versions, even if the versions are equal.
    Please add the Kotlin plugin to the common parent project or
    the root project, then remove the versions in the subprojects.
    ```
    
    PiperOrigin-RevId: 896505206

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,13 @@ buildscript {
   }
 }
 
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose.compiler) apply false
+}
+
 allprojects {
   repositories {
     google()
