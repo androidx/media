7a25bc1550ec07d8cb2991d4bf3e13719ec961f9
kimvde <kimvde@google.com>
2026-05-21 03:00:46 -0700

    Fix samples exceeding duration when transmuxing clipped MediaItems.
    PiperOrigin-RevId: 918938878

--- libraries/transformer/src/test/java/androidx/media3/transformer/EncodedSampleExporterTest.java
+++ libraries/transformer/src/test/java/androidx/media3/transformer/EncodedSampleExporterTest.java
@@ -15,6 +15,7 @@
  */
 package androidx.media3.transformer;
 
+import static androidx.media3.common.C.BUFFER_FLAG_END_OF_STREAM;
 import static androidx.media3.transformer.EditedMediaItemSequence.withAudioFrom;
 import static androidx.media3.transformer.EncodedSampleExporter.ALLOCATION_SIZE_TARGET_BYTES;
 import static androidx.media3.transformer.EncodedSampleExporter.MAX_INPUT_BUFFER_COUNT;
@@ -34,6 +35,7 @@ import androidx.media3.common.util.ListenerSet;
 import androidx.media3.decoder.DecoderInputBuffer;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.common.collect.ImmutableList;
+import java.nio.ByteBuffer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,6 +45,8 @@ import org.mockito.Mock;
 @RunWith(AndroidJUnit4.class)
 public final class EncodedSampleExporterTest {
 
+  private EditedMediaItem emptyEditedMediaItem;
+  private Format format;
   private EncodedSampleExporter encodedSampleExporter;
 
   @Mock private ListenerSet.IterationFinishedEvent<Transformer.Listener> mockIterationFinishedEvent;
@@ -50,20 +54,19 @@ public final class EncodedSampleExporterTest {
 
   @Before
   public void setUp() {
+    emptyEditedMediaItem = new EditedMediaItem.Builder(MediaItem.EMPTY).build();
+    format = new Format.Builder().setSampleMimeType(MimeTypes.AUDIO_AAC).build();
     Looper looper = checkNotNull(Looper.myLooper());
     FallbackListener fallbackListener =
         new FallbackListener(
-            new Composition.Builder(
-                    withAudioFrom(
-                        ImmutableList.of(new EditedMediaItem.Builder(MediaItem.EMPTY).build())))
-                .build(),
+            new Composition.Builder(withAudioFrom(ImmutableList.of(emptyEditedMediaItem))).build(),
             new ListenerSet<>(looper, Clock.DEFAULT, mockIterationFinishedEvent),
             mockHandlerWrapper,
             new TransformationRequest.Builder().build());
     fallbackListener.setTrackCount(1);
     encodedSampleExporter =
         new EncodedSampleExporter(
-            new Format.Builder().setSampleMimeType(MimeTypes.AUDIO_AAC).build(),
+            format,
             new TransformationRequest.Builder().build(),
             new MuxerWrapper(
                 /* outputPath= */ "unused",
@@ -78,6 +81,13 @@ public final class EncodedSampleExporterTest {
 
   @Test
   public void queueInput_withEmptyBuffers_allocatesMaxBufferCount() {
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
     for (int i = 0; i < MAX_INPUT_BUFFER_COUNT; i++) {
       @Nullable DecoderInputBuffer decoderInputBuffer = encodedSampleExporter.getInputBuffer();
       assertThat(decoderInputBuffer).isNotNull();
@@ -89,24 +99,52 @@ public final class EncodedSampleExporterTest {
 
   @Test
   public void queueInput_withSmallBuffers_allocatesMaxBufferCount() {
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
     assertThat(fillInputAndGetTotalInputSize(/* inputBufferSizeBytes= */ 1))
         .isEqualTo(MAX_INPUT_BUFFER_COUNT);
   }
 
   @Test
   public void queueInput_withMediumBuffers_reachesBufferSizeTarget() {
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
     assertThat(fillInputAndGetTotalInputSize(/* inputBufferSizeBytes= */ 16 * 1024))
         .isEqualTo(ALLOCATION_SIZE_TARGET_BYTES);
   }
 
   @Test
   public void queueInput_withLargeBuffers_allocatesMinBufferCount() {
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
     assertThat(fillInputAndGetTotalInputSize(/* inputBufferSizeBytes= */ 1024 * 1024))
         .isEqualTo(MIN_INPUT_BUFFER_COUNT * 1024 * 1024);
   }
 
   @Test
   public void queueInputToLimitThenProcessOutput_queueInputSucceeds() {
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
     // Queue input until no more input is accepted.
     assertThat(fillInputAndGetTotalInputSize(/* inputBufferSizeBytes= */ 16 * 1024))
         .isEqualTo(ALLOCATION_SIZE_TARGET_BYTES);
@@ -120,6 +158,83 @@ public final class EncodedSampleExporterTest {
     // It's possible to queue input again.
     assertThat(fillInputAndGetTotalInputSize(/* inputBufferSizeBytes= */ 16 * 1024))
         .isEqualTo(ALLOCATION_SIZE_TARGET_BYTES);
+  }
+
+  @Test
+  public void queueInput_withMultipleMediaItems_skipsBuffersExceedingDuration() {
+    // Initialize with the first media item, not the last.
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ false,
+        /* positionOffsetUs= */ 0);
+
+    // Buffer within duration
+    DecoderInputBuffer buffer1 = encodedSampleExporter.getInputBuffer();
+    buffer1.timeUs = 50;
+    buffer1.data = ByteBuffer.allocate(10);
+    encodedSampleExporter.queueInputBuffer();
+
+    // Buffer exceeding duration - This buffer and all upcoming buffer from the same MediaItem
+    // should be skipped (in case they reference this buffer).
+    DecoderInputBuffer buffer2 = encodedSampleExporter.getInputBuffer();
+    buffer2.timeUs = 150;
+    buffer2.data = ByteBuffer.allocate(10);
+    encodedSampleExporter.queueInputBuffer();
+
+    // Buffer within duration - Should be skipped
+    DecoderInputBuffer buffer3 = encodedSampleExporter.getInputBuffer();
+    buffer3.timeUs = 75;
+    buffer3.data = ByteBuffer.allocate(10);
+    encodedSampleExporter.queueInputBuffer();
+
+    // Drain and check timestamps
+    DecoderInputBuffer outputBuffer = encodedSampleExporter.getMuxerInputBuffer();
+    assertThat(outputBuffer).isSameInstanceAs(buffer1);
+    assertThat(outputBuffer.timeUs).isEqualTo(50);
+    encodedSampleExporter.releaseMuxerInputBuffer();
+
+    assertThat(encodedSampleExporter.getMuxerInputBuffer()).isNull();
+
+    // Now add the second media item (which is the last). nextMediaItemOffsetUs becomes 100.
+    encodedSampleExporter.onMediaItemChanged(
+        emptyEditedMediaItem,
+        /* durationUs= */ 100,
+        format,
+        /* isLast= */ true,
+        /* positionOffsetUs= */ 0);
+
+    // Buffer time 50, so final time is 100 + 50 = 150
+    DecoderInputBuffer buffer4 = encodedSampleExporter.getInputBuffer();
+    buffer4.timeUs = 50;
+    buffer4.data = ByteBuffer.allocate(10);
+    encodedSampleExporter.queueInputBuffer();
+
+    // Buffer time 150, so final time is 100 + 150 = 250. Not skipped as it's the last item.
+    DecoderInputBuffer buffer5 = encodedSampleExporter.getInputBuffer();
+    buffer5.timeUs = 150;
+    buffer5.data = ByteBuffer.allocate(10);
+    encodedSampleExporter.queueInputBuffer();
+
+    // End-of-stream buffer
+    DecoderInputBuffer buffer6 = encodedSampleExporter.getInputBuffer();
+    buffer6.addFlag(BUFFER_FLAG_END_OF_STREAM);
+    encodedSampleExporter.queueInputBuffer();
+
+    // Drain and check timestamps
+    outputBuffer = encodedSampleExporter.getMuxerInputBuffer();
+    assertThat(outputBuffer).isSameInstanceAs(buffer4);
+    assertThat(outputBuffer.timeUs).isEqualTo(150);
+    encodedSampleExporter.releaseMuxerInputBuffer();
+
+    outputBuffer = encodedSampleExporter.getMuxerInputBuffer();
+    assertThat(outputBuffer).isSameInstanceAs(buffer5);
+    assertThat(outputBuffer.timeUs).isEqualTo(250);
+    encodedSampleExporter.releaseMuxerInputBuffer();
+
+    assertThat(encodedSampleExporter.getMuxerInputBuffer()).isNull();
+    assertThat(encodedSampleExporter.isMuxerInputEnded()).isTrue();
   }
 
   private long fillInputAndGetTotalInputSize(int inputBufferSizeBytes) {
