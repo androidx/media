144c5e9cf8a5655274a9de8b06ea10ca3cba7c33
tonihei <tonihei@google.com>
2026-07-01 10:49:15 -0700

    Ignore flaky test
    #cherrypick
    
    PiperOrigin-RevId: 941184081

--- libraries/effect/src/test/java/androidx/media3/effect/DefaultGlFrameProcessorTest.java
+++ libraries/effect/src/test/java/androidx/media3/effect/DefaultGlFrameProcessorTest.java
@@ -59,6 +59,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -522,6 +523,7 @@ public final class DefaultGlFrameProcessorTest {
     assertThat(glTextureFramesReleased.get()).isEqualTo(3);
   }
 
+  @Ignore // TODO: b/529360695 - Re-enable when no longer flaky.
   @Test
   public void queue_withFrameWriterRejectingInput_buffersAndRetriesCorrectly() throws Exception {
     // batch A
