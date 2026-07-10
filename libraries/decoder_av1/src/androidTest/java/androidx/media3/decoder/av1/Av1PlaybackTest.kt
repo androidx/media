0d6779160358d5b1eb93164e400a9a61e47221d2
tonihei <tonihei@google.com>
2026-07-10 05:19:09 -0700

    Rollback of https://github.com/androidx/media/commit/46eaeee453de9eb9b279423dde2b186c9ea47dc1
    PiperOrigin-RevId: 945654742
    (cherry picked from commit 9f2157e02dc38a2cec257634f4eff26621bd3bae)

--- libraries/decoder_av1/src/androidTest/java/androidx/media3/decoder/av1/Av1PlaybackTest.kt
+++ libraries/decoder_av1/src/androidTest/java/androidx/media3/decoder/av1/Av1PlaybackTest.kt
@@ -16,8 +16,6 @@
 package androidx.media3.decoder.av1
 
 import android.content.Context
-import android.graphics.SurfaceTexture
-import android.view.Surface
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
@@ -25,12 +23,9 @@ import androidx.media3.exoplayer.RenderersFactory
 import androidx.media3.exoplayer.video.VideoDecoderGLSurfaceView
 import androidx.media3.test.utils.awaitPlaybackState
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.filters.SdkSuppress
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
-import com.google.testing.junit.testparameterinjector.KotlinTestParameters.namedTestValues
-import com.google.testing.junit.testparameterinjector.TestParameter
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -38,7 +33,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 /** Playback tests using [Libdav1dVideoRenderer]. */
-@RunWith(TestParameterInjector::class)
+@RunWith(AndroidJUnit4::class)
 class Av1PlaybackTest {
 
   @Before
@@ -47,67 +42,37 @@ class Av1PlaybackTest {
   }
 
   @Test
-  fun playback_rendersToGlSurfaceView(
-    @TestParameter
-    videoUri: String =
-      namedTestValues("DEFAULT" to SAMPLE_AV1_URI, "MONOCHROME" to SAMPLE_AV1_MONO_URI)
-  ) =
+  fun basicPlayback() =
     runBlocking(Dispatchers.Main) {
-      runPlaybackTest(videoUri) { player ->
-        player.setVideoSurfaceView(VideoDecoderGLSurfaceView(getApplicationContext()))
-      }
-    }
-
-  // Rendering using ANativeWindow_lock is only reliably supported on API 29+.
-  @Test
-  @SdkSuppress(minSdkVersion = 29)
-  fun playback_rendersToSurface(
-    @TestParameter
-    videoUri: String =
-      namedTestValues("DEFAULT" to SAMPLE_AV1_URI, "MONOCHROME" to SAMPLE_AV1_MONO_URI)
-  ) =
-    runBlocking(Dispatchers.Main) {
-      val surfaceTexture = SurfaceTexture(/* texName= */ 0)
-      val surface = Surface(surfaceTexture)
-      try {
-        runPlaybackTest(videoUri) { player -> player.setVideoSurface(surface) }
-      } finally {
-        surface.release()
-        surfaceTexture.release()
-      }
-    }
-
-  private suspend fun runPlaybackTest(videoUri: String, setSurfaceAction: (ExoPlayer) -> Unit) {
-    val context = getApplicationContext<Context>()
-    val renderersFactory = RenderersFactory { eventHandler, videoRendererEventListener, _, _, _ ->
-      arrayOf(
-        Libdav1dVideoRenderer(
-          /* allowedJoiningTimeMs= */ 0,
-          eventHandler,
-          videoRendererEventListener,
-          /* maxDroppedFramesToNotify= */ -1,
+      val context = getApplicationContext<Context>()
+      val renderersFactory = RenderersFactory { eventHandler, videoRendererEventListener, _, _, _ ->
+        arrayOf(
+          Libdav1dVideoRenderer(
+            /* allowedJoiningTimeMs= */ 0,
+            eventHandler,
+            videoRendererEventListener,
+            /* maxDroppedFramesToNotify= */ -1,
+          )
         )
-      )
-    }
-    val player = ExoPlayer.Builder(context).setRenderersFactory(renderersFactory).build()
-    setSurfaceAction(player)
-    player.setMediaItem(MediaItem.fromUri(videoUri))
-    player.prepare()
-    player.play()
-
-    val renderedOutputBufferCount =
-      try {
-        player.awaitPlaybackState(Player.STATE_ENDED)
-        player.videoDecoderCounters?.renderedOutputBufferCount ?: 0
-      } finally {
-        player.release()
       }
+      val player = ExoPlayer.Builder(context).setRenderersFactory(renderersFactory).build()
+      player.setVideoSurfaceView(VideoDecoderGLSurfaceView(context))
+      player.setMediaItem(MediaItem.fromUri(SAMPLE_AV1_URI))
+      player.prepare()
+      player.play()
 
-    assertThat(renderedOutputBufferCount).isAtLeast(1)
-  }
+      val renderedOutputBufferCount =
+        try {
+          player.awaitPlaybackState(Player.STATE_ENDED)
+          player.videoDecoderCounters?.renderedOutputBufferCount ?: 0
+        } finally {
+          player.release()
+        }
+
+      assertThat(renderedOutputBufferCount).isAtLeast(1)
+    }
 
   companion object {
     private const val SAMPLE_AV1_URI = "asset:///media/mp4/sample_av1.mp4"
-    private const val SAMPLE_AV1_MONO_URI = "asset:///media/mp4/sample_av1_mono.mp4"
   }
 }
