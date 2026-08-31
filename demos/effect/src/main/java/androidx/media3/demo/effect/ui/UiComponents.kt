0f4054759c7579d6d5ae19275af5de7b096f62c5
Googler <noreply@google.com>
2026-08-31 06:25:23 -0700

    ...G3 library drop...
    PiperOrigin-RevId: 973846087

--- demos/effect/src/main/java/androidx/media3/demo/effect/ui/UiComponents.kt
+++ demos/effect/src/main/java/androidx/media3/demo/effect/ui/UiComponents.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
